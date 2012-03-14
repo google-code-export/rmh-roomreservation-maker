d3ce41d3118d1488a6e19c9f64876c220726ac3e
MPagliuca90@gmail.com <MPagliuca90@gmail.com@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-03-14 17:25:39 +0000

    new brreds added

--- trunk/Lab7-2012A/CatListFun.php
+++ trunk/Lab7-2012A/CatListFun.php
@@ -2,18 +2,31 @@
           include_once 'Cat.php';
           
           $cat1 = new Cat;
+          $cat1->setBreed("Abyssinian");
           $cat1->setColor("black");
           $cat1->setname("Zoe");
           $myCats[] = $cat1;
           $cat1->displayCat();
           
         $cat2 = new Cat;
+          $cat2->setBreed("Toyger");
+          $cat1->setColor("orange");
+          $cat1->setname("Garfield");
+          $myCats[] = $cat1;
+
           $cat2->setColor("orange");
           $cat2->setname("Garfield");
           $myCats[] = $cat2;
           $cat2->displayCat();
 
+
            $cat3 = new Cat;
+          $cat3->setBreed("Sphynx");
+          $cat1->setColor("tabby");
+          $cat1->setname("Fluffy");
+          $myCats[] = $cat1;
+          
+          
           $cat3->setColor("tabby");
           $cat3->setname("Fluffy");
           $myCats[] = $cat3;
