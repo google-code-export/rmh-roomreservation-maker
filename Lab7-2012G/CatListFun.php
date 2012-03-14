f2f34866133a5785bab617ea620b02a908505433
paul.kubler10@stjohns.edu <paul.kubler10@stjohns.edu@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-03-14 16:54:50 +0000

    Added Breed

--- Lab7-2012G/CatListFun.php
+++ Lab7-2012G/CatListFun.php
@@ -3,17 +3,20 @@
           $cat1 = new Cat;
           $cat1->setColor("black");
           $cat1->setname("Zoe");
+          $cat1->setBreed("Abyssinian");
           $myCats[] = $cat1;
           
-        $cat2 = new Cat;
-          $cat1->setColor("orange");
-          $cat1->setname("Garfield");
-          $myCats[] = $cat1;
+          $cat2 = new Cat;
+          $cat2->setColor("orange");
+          $cat2->setname("Garfield");
+          $cat2->setBreed("Cymric");
+          $myCats[] = $cat2;
           
-           $cat3 = new Cat;
-          $cat1->setColor("tabby");
-          $cat1->setname("Fluffy");
-          $myCats[] = $cat1;
+          $cat3 = new Cat;
+          $cat3->setColor("tabby");
+          $cat3->setname("Fluffy");
+          $cat3->setBreed("Pixie-bob");
+          $myCats[] = $cat3;
          
 function countCats($cats, $color)
 {
