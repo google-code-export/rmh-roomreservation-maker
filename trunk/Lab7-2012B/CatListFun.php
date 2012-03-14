a46f9805c5027b1a68765cfafb255997047c40f8
kellyshoemakerr@gmail.com <kellyshoemakerr@gmail.com@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-03-14 16:23:57 +0000

    breed changes

--- trunk/Lab7-2012B/CatListFun.php
+++ trunk/Lab7-2012B/CatListFun.php
@@ -3,15 +3,18 @@
           $cat1 = new Cat;
           $cat1->setColor("black");
           $cat1->setname("Zoe");
+          $cat1->setBreed("Persian");
           $myCats[] = $cat1;
           
         $cat2 = new Cat;
           $cat1->setColor("orange");
           $cat1->setname("Garfield");
+          $cat1->setBreed("Siamese");
           $myCats[] = $cat1;
           
            $cat3 = new Cat;
           $cat1->setColor("tabby");
           $cat1->setname("Fluffy");
+          $cat1->setBreed("Abyssinian");
           $myCats[] = $cat1;
 ?>
