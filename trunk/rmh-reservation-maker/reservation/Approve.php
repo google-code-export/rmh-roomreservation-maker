3f41ea0753b2b3b045adfbe03c8efac06e1c44d3
paul.kubler10@stjohns.edu <paul.kubler10@stjohns.edu@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-04 16:56:48 +0000

    Currently commented out as db functions are needed and will be added.

---
+++ trunk/rmh-reservation-maker/reservation/Approve.php
@@ -0,0 +1,9 @@
+<?php
+
+//Append "-confirmed" to status
+get_status()+='-confirmed';
+//Submit Changes to database
+//Generate Key ID
+//Send automatic email to SW
+
+?>  
---
+++ trunk/rmh-reservation-maker/reservation/Deny.php
@@ -0,0 +1,9 @@
+<?php
+
+//Append "-denied" to status
+get_status()+='-denied';
+//Submit Changes to database
+//Generate Key ID
+//Send automatic email to SW
+
+?>
