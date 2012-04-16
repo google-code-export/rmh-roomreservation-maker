1b99b803770cf19364961fe5a88e96cef710aa8e
ger100@gmail.com <ger100@gmail.com@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-16 05:11:54 +0000

    Profile Activity domain object with update variables from the ERD

--- trunk/rmh-reservation-maker/domain/ProfileActivity.php
+++ trunk/rmh-reservation-maker/domain/ProfileActivity.php
@@ -21,12 +21,32 @@ class ProfileActivity {
     private $activityType; //string: utilized by the social worker for the profile activity request: 'Apply','Modify','Cancel'
     private $profileActivityStatus; //string: utilized by the rmh staff for the profile activity request: 'Unconfirmed','Confirm','Deny'
     private $profileActivityNotes; //(optional) notes from the rmh staff/social worker
-        /**
+    private $parentFirstName;
+    private $parentLastName;
+    private $parentEmail;
+    private $parentPhone1;
+    private $parentPhone2;
+    private $parentAddress;
+    private $parentCity;
+    private $parentState;
+    private $parentZip;
+    private $parentCountry;
+    private $patientFirstName;
+    private $patientLastName;
+    private $patientRelation;
+    private $patientDOB;
+    private $formPdf;
+    private $familyNotes;
+    
+    /**
          * constructor for profile activity
          */
     function __construct($profileActivityId, $profileActivityRequestId, $socialWorkerProfileId,
             $rmhStaffProfileId, $familyProfileId,$swDateStatusSubm,$rmhDateStatusSubm,
-            $activityType, $profileActivityStatus,$profileActivityNotes){                
+            $activityType, $profileActivityStatus,$profileActivityNotes, $parentFirstName, $parentLastName,
+            $parentEmail, $parentPhone1, $parentPhone2, $parentAddress, $parentCity, $parentState, $parentZip, 
+            $parentCountry, $patientFirstName, $patientLastName, $patientRelation, $patientDOB, $formPdf,
+            $familyNotes){                
         
         $this->profileActivityId = $profileActivityId;
         $this->profileActivityRequestId = $profileActivityRequestId;
@@ -38,7 +58,22 @@ class ProfileActivity {
         $this->activityType = $activityType;  
         $this->profileActivityStatus = $profileActivityStatus; 
         $this->profileActivityNotes = $profileActivityNotes;   
-    
+        $this->parentFirstName = $parentFirstName;
+        $this->parentLastName = $parentLastName;
+        $this->parentEmail = $parentEmail;
+        $this->parentPhone1 = $parentPhone1;
+        $this->parentPhone2 = $parentPhone2;
+        $this->parentAddress = $parentAddress;
+        $this->parentCity= $parentCity;
+        $this->parentState = $parentState;
+        $this->parentZip = $parentZip;
+        $this->parentCountry=$parentCountry;
+        $this->patientFirstName = $patientFirstName;
+        $this->patientLastName = $patientLastName;
+        $this->patientRelation = $patientRelation;
+        $this->patientDOB = $patientDOB;
+        $this->formPdf = $formPdf;
+        $this->familyNotes=$familyNotes;
     //getter functions
        
     function get_profileActivityId(){
@@ -71,6 +106,56 @@ class ProfileActivity {
     function get_profileActivityNotes(){
     return $this->profileActivityNotes;}
     
+    function get_parentFirstName(){
+    return $this->parentFirstName;}
+    
+    function get_parentLastName(){
+    return $this->parentLastName;}
+    
+    function get_parentEmail(){
+    return $this->parentEmail;}
+    
+    function get_parentPhone1(){
+    return $this->parentPhone1;}
+    
+    function get_parentPhone2(){
+    return $this->parentPhone2;}
+    
+    function get_parentAddress(){
+    return $this->parentAddress;}
+    
+    function get_parentCity(){
+    return $this->parentCity;}
+    
+    function get_parentState(){
+    return $this->parentState;}
+    
+    function get_parentZip(){
+    return $this->parentZip;}
+    
+    function get_parentCountry(){
+    return $this->parentCountry;}
+    
+    function get_patientFirstName(){
+    return $this->patientFirstName;}
+    
+    function get_patientLastName(){
+    return $this->patientLastName;}
+    
+    function get_patientRelation(){
+    return $this->patientRelation;}
+    
+    function get_patientDOB(){
+    return $this->patientDOB;}
+    
+    function get_formPdf(){
+    return $this->formPdf;}
+    
+    function get_familyNotes(){
+    return $this->familyNotes;}
+    
+
+    
     //setter functions
     
     function set_profileActivityId($profActivityId){
@@ -102,6 +187,55 @@ class ProfileActivity {
     
     function set_profileActivityNotes($profActivityNotes){
     $this->profileActivityNotes = $profActivityNotes;}
+    
+    function set_parentFirstName($parFirstName){
+    $this->parentFirstName = $parFirstName;}
+    
+    function set_parentLastName($parLastName){
+    $this->parentLastName = $parLastName;}
+    
+    function set_parentEmail($parEmail){
+    $this->parentEmail = $parEmail;}
+    
+    function set_parentPhone1($parPhone1){
+    $this->parentPhone1 = $parPhone1;}
+    
+    function set_parentPhone2($parPhone2){
+    $this->parentPhone2 = $parPhone2;}
+    
+    function set_parentAddress($parAddr){
+    $this->parentAddress = $parAddr;}
+    
+    function set_parentCity($parCity){
+    $this->parentCity = $parCity;}
+    
+    function set_parentState($parState){
+    $this->parentState = $parState;}
+    
+    function set_parentZip($parZip){
+    $this->parentZip = $parZip;}
+    
+    function set_parentCountry($parCountry){
+    $this->parentCountry = $parCountry;}
+    
+    function set_patientFirstName($patFirstName){
+    $this->patientFirstName = $patFirstName;}
+    
+    function set_patientLastName($patLastName){
+    $this->patientLastName = $patLastName;}
+    
+    function set_patientRelation($patRel){
+    $this->patientRelation = $patRel;}
+    
+    function set_patientDOB($patDOB){
+    $this->patientDOB = $patDOB;}
+    
+    function set_formPdf($pdfForm){
+    $this->formPdf = $pdfForm;}
+    
+    function set_familyNotes($famNotes){
+    $this->familyNotes=$famNotes;}
+    
 }
 }
 
