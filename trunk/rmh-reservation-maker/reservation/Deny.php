b7015a0793824bb90bd140b4cba0a9b518d338b6
paul.kubler10@stjohns.edu <paul.kubler10@stjohns.edu@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-18 17:03:25 +0000

    Slightly updated Deny. Adds email function names, though waiting upon further specification.

--- trunk/rmh-reservation-maker/reservation/Deny.php
+++ trunk/rmh-reservation-maker/reservation/Deny.php
@@ -1,9 +1,15 @@
 <?php
-
+/*
+  * @author Paul Kubler
+  * 
+  * Approver Form View
+  * 
+  */
 //Append "-denied" to status
 get_status()+='-denied';
 //Submit Changes to database
 //Generate Key ID
 //Send automatic email to SW
-
+ModifyDeny($RequestKey, $SWName, $familyLname, $DateToAndFrom);
+RequestDeny($RequestKey, $SWName, $familyLname, $DateToAndFrom);
 ?>
