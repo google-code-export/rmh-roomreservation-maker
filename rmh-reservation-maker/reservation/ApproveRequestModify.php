6e948c38987f1d77b9c553ee69244a8506b91cbc
paul.kubler10@stjohns.edu <paul.kubler10@stjohns.edu@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-18 17:14:09 +0000

    Created/Renamed new functions to deal with specific events/details.

--- rmh-reservation-maker/reservation/ApproveRequestModify.php
+++ rmh-reservation-maker/reservation/ApproveRequestModify.php
@@ -1,7 +1,13 @@
 <?php
-
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+  * @author Paul Kubler
+  * 
+  * Approve Request Modify
+  * 
+  */
+//Append "-confirmed" to status
+get_status()+='-confirmed';
+//Submit Changes to database
+//Generate Key ID
+ModifyAccept($RequestKey, $SWName, $familyLname, $DateToAndFrom);
 ?>
