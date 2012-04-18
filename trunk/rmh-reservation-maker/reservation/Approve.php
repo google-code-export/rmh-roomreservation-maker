151948e8e3faebe9ef06b98958bd0cec635fcaed
paul.kubler10@stjohns.edu <paul.kubler10@stjohns.edu@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-18 17:03:10 +0000

    Slightly updated Approve. Adds email function names, though waiting upon further specification.

--- trunk/rmh-reservation-maker/reservation/Approve.php
+++ trunk/rmh-reservation-maker/reservation/Approve.php
@@ -1,9 +1,16 @@
 <?php
-
+/*
+  * @author Paul Kubler
+  * 
+  * Approver Form View
+  * 
+  */
 //Append "-confirmed" to status
 get_status()+='-confirmed';
 //Submit Changes to database
 //Generate Key ID
-//Send automatic email to SW
+RequestAccept($RequestKey, $SWName, $familyLname, $DateToAndFrom);
 
-?>  
+ModifyAccept($RequestKey, $SWName, $familyLname, $DateToAndFrom);
+
+?>
