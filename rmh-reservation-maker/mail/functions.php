58533fa9ba36e353ea55dc2abf3357a3c107ab10
alisa.modeste08@stjohns.edu <alisa.modeste08@stjohns.edu@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-18 17:02:59 +0000

    Updated the first four functions. Mail can currently only be sent if both the sender's and recipient's address on the safe list.

--- rmh-reservation-maker/mail/functions.php
+++ rmh-reservation-maker/mail/functions.php
@@ -3,33 +3,40 @@
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
+ * 
+ * TO DO
+ * Test to see if an email can be sent to multiple addresses using mail()
+ * can be done using PEAR Mail
+ * 
+ * Authors: Alisa Modeste <alisa.modeste08@stjohns.edu>
  */
 
 include_once ('../core/config.php');
 include_once (ROOT_DIR.'/database/dbUserProfile.php');
 
 
-//allows you to modify settings and check if mail was sent
-//To do:
-//rewrite to allow for sending to multiple addresses
-//edit from
-function email($to, $subject, $message)
+//email allows you to modify settings and check if mail was sent
+//@param array $to one or more recipients for the email
+
+function email($add, $subject, $message)
 {
     $from = 'alisa.modeste08@stjohns.edu'; //this SHOULD be a VALID email address
 
     ini_set("SMTP","mailhubout.stjohns.edu");
     ini_set('sendmail_from', $from);
     ini_set("smtp_port","25");
+    
+     foreach ($add as $to):
+        $mailed = mail($to, $subject, $message);
 
+        if($mailed):
+            echo 'The email was sent';
+        else:
+            echo 'The email could not be sent, please try again';
+        endif;
+    endforeach;
 
-    $mailed = mail($to, $subject, $message);
-
-    if($mailed):
-        echo 'The email was sent';
-    else:
-        echo 'The email could not be sent, please try again';
-    endif;
-
+   
 }
 
 
@@ -39,7 +46,7 @@ function ConfirmCancel($RequestKey, $SWID, $familyLname, $DateToAndFrom)
     $SW = retrieve_UserProfile_SW($SWID);
    
  //   if ($SW[0]->get_email_notification() == 'yes'):
-        $to = $SW[0]->get_email();
+        $to[] = $SW[0]->get_email();
         
         $subject = "Confirmation of Cancelled Request";
 
@@ -58,7 +65,7 @@ function ModifyDeny($RequestKey, $SWID, $familyLname, $DateToAndFrom, $note = ""
     $SW = retrieve_UserProfile_SW($SWID);
    
    // if ($SW[0]->get_email_notification() == 'yes'):
-        $to = $SW[0]->get_email();
+        $to[] = $SW[0]->get_email();
     
         $subject = "Cannot Accommodate Modified Request";
         
@@ -80,7 +87,7 @@ function ModifyAccept($RequestKey, $SWID, $familyLname, $DateToAndFrom)
     $SW = retrieve_UserProfile_SW($SWID);
    
  //  if ($SW[0]->get_email_notification() == 'Yes'):
-        $to = $SW[0]->get_email();
+        $to[] = $SW[0]->get_email();
         
         $subject = "Modified Request has been Accepted";
 
