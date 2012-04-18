b617936d42c7416de1e758ac322464bbc23c21f3
lindashek1@gmail.com <lindashek1@gmail.com@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-18 15:56:47 +0000

    (Corrections) to Create Function.

--- rmh-reservation-maker/database/dbFamilyProfile.php
+++ rmh-reservation-maker/database/dbFamilyProfile.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once(dirname(__FILE__).'/../domain/Family.php');
+include_once(dirname(__FILE__).'/domain/Family.php');
 include_once(dirname(__FILE__).'/dbinfo.php');
 
 function create_dbFamilyProfile() 
