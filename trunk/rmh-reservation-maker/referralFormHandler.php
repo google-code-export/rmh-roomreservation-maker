70c07787f891253f8a1442cfeb9af8c86b9a536e
ggame101 <ggame101@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-20 02:54:57 +0000

    testing the if statements

--- trunk/rmh-reservation-maker/referralFormHandler.php
+++ trunk/rmh-reservation-maker/referralFormHandler.php
@@ -17,80 +17,154 @@ include_once (ROOT_DIR . '/database/dbFamilyProfile.php');
 
 
 
-/*
+ $socialWorkerProfileId = $_SESSION['id'];
+ 
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ /*
  * Submitted Form Actions if a token is set, and $_POST['submit'] == "submit"
  */
     if( isset( $_POST['form_token'] ) && validateTokenField( $_POST ) )
     {
         if( $_POST['submit'] == "submit" )//test the submission of a room Referral
         {
-            if( isset( $_POST["BeginDate"] ) )
+            $referralform = true;
+            
+            if( isset( $_POST["BeginDate"] ) ){
                 $newBeginDate = $sanitize( $_POST['BeginDate'] );
-                $newBeginDate = $new_referral;
+            }
+                
                 
 
-            if( isset( $_POST["EndDate"] ) )
+            if( isset( $_POST["EndDate"] ) ){
                 $newEndDate = $sanitize( $_POST["EndDate"] ); 
-                $newEndDate = $new_referral;
+            }
+                
                 
             
-            if( isset( $_POST["PatientDiagnosis"] ) )
+            if( isset( $_POST["PatientDiagnosis"] ) ){
                 $newPatientDiagnosis = $sanitize( $_POST["PatientDiagnosis"] );
-                $newPatientDiagnosis = $new_referral;
+            }
             
-            if( isset( $_POST["Notes"] ) )
+            if( isset( $_POST["Notes"] ) ){
                 $newNotes = $sanitize( $_POST["Notes"] );
-                $newNotes = $new_referral;
+            }
+                
             
-            if( isset( $_POST["ParentLastName"] ) )
+            if( isset( $_POST["ParentLastName"] ) ){
                 $newParentLastName = $sanitize( $_POST["ParentLastName"] );
-                $newParentLastName = $new_referral;
+            }
+                
             
-            if( isset( $_POST["ParentFirstName"] ) )
+            if( isset( $_POST["ParentFirstName"] ) ){
                 $newParentFirstName = $sanitize( $_POST["ParentFirstName"] );
-                $newParentFirstName = $new_referral;
+            }
+                
             
-            $new_referral = new Requests( );
-            insert_RoomReservationActivity( $new_referral );
+            $myReservation = new Reservation($roomReservationActivityID, $roomReservationRequestID, $socialWorkerProfileId, $rmhStaffProfileId,
+            $familyProfileId,$activityType, $status, $swDateStatusSubmitted, $rmhDateStatusSubmitted, $beginDate, $endDate, 
+            $patientDiagnosis, $roomnote);
+            $myReservation = insert_RoomReservationActivity( );
             
         } //end if( $_POST['submit'] == "submit" )
        
     }
     else if(isset($_POST['form_token']) && !validateTokenField($_POST))
     {
-        //if the security validation failed. display/store the error:
-        //'The request could not be completed: security check failed!'
+        $referralform = false; //if the security validation failed. display/store the error:
+                          //'The request could not be completed: security check failed!'
 
     }
     else
     {
-        //there was no POST DATA
+        $referralform = false; //there was no POST DATA
     }
-        
+    
+       $errorMessage = "";
+         //check that the fields are not blank and tell the user to fill them in
 
-?>
+       if ( empty ($newBeginDate) ){
 
-<div id="container">
+           $errorMessage .= " Please enter a BeginDate. ";
 
-    <div id="content">
-        
+       }
+
+       
+       if ( empty ($newEndDate)  ){
+
+           $errorMessage .= " Please enter a EndDate. ";
+
+       } 
+       
+       if ( empty ($newPatientDiagnosis) ){
+           
+           $errorMessage .= " Please enter the Patient's Diagnosis. ";
+       }
+       
+       if ( empty ($newParentLastName) ){
+           
+           $errorMessage .= " Please enter the Parent's Last Name. ";
+       }
+       
+       if ( empty ($newParentFirstName) ){
+           
+           $errorMessage .= " Please enter the Parent's First Name. ";
+       }
+               
+
+    
+    
+    
+   $new_referral->beginDate = $newBeginDate;
+   $new_referral->endDate = $newEndDate;
+   $new_referral->patientDiagnosis = $newPatientDiagnosis;
+   $new_referral->roomnote = $newNotes; 
+   $new_referral->RoomReservationRequestID = $RoomReservationRequestID;
+   $new_referral->ActivityType = $ActivityType;
    
-            
-        }
-    }
-    else
-    {
-    echo "Security check failed: Go back to the form and resubmit your selections<br>";
-    }
+   
+   $RoomReservationRequestID = "Unconfirmed";
+   $ActivityType ="Apply";
+
+   
+    //confirming to the social worker that the form went through
+    echo "<br>";
+    echo "Room Request made by: ".$socialWorkerProfileId."<br>";
+    echo "Begin Date: ".$newBeginDate."<br>";
+    echo "End Date: ".$newEndDate."<br>";
+    
+    
+    mail("approvers email address goes here", $RoomReservationRequestID, "This is a pending request")//email the approver the request key, not sure if it should look like this though.
+    
+    
+    
+ 
+    
+    
+    
+    
+    
+
 ?>
-        <br>
-        
-        
 
-    </div>
-</div>
-
-<?php     
+    <?php     
 include (ROOT_DIR.'/footer.php');
 ?>
 
