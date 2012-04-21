f756b546e50fecb95c2cd5bd5766ce72ec62560a
lindashek1@gmail.com <lindashek1@gmail.com@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-21 00:36:35 +0000

    (Modified) Reservation class. Constructor should match the new created object in dbReservation.php.

--- rmh-reservation-maker/domain/Reservation.php
+++ rmh-reservation-maker/domain/Reservation.php
@@ -1,120 +1,212 @@
 <?php
+/**
+ * Reservation class for RMH Room Reservation Maker. 
+ * @version April 20, 2012
+ * @author Linda Shek and Gergana Stoykova
+ */
 
-include_once(ROOT_DIR .'/domain/UserProfile.php');
-include_once(ROOT_DIR .'/domain/Family.php');
+//include_once(ROOT_DIR .'/domain/UserProfile.php');
+//include_once(ROOT_DIR .'/domain/Family.php');
 class Reservation  {
    
     private $roomReservationActivityID;
     private $roomReservationRequestID;
-    private $socialWorkerProfileId;
-    private $rmhStaffProfileId;
     private $familyProfileId;
-    private $activityType;
-    private $status;
+    private $parentLastName;
+    private $parentFirstName;
+    private $socialWorkerProfileId;
+    private $swLastName;
+    private $swFirstName;
+    private $rmhStaffProfileId;
+    private $rmhStaffLastName;
+    private $rmhStaffFirstName;       
     private $swDateStatusSubmitted;
     private $rmhDateStatusSubmitted;
+    private $activityType;
+    private $status;
     private $beginDate;
     private $endDate;
     private $patientDiagnosis;
     private $roomnote;
     
-    function __construct($roomReservationActivityID, $roomReservationRequestID, $socialWorkerProfileId, $rmhStaffProfileId,
-            $familyProfileId,$activityType, $status, $swDateStatusSubmitted, $rmhDateStatusSubmitted, $beginDate, $endDate, 
-            $patientDiagnosis, $roomnote){
+      
+    function __construct($roomReservationActivityID, $roomReservationRequestID, $familyProfileId, $parentLastName, 
+                $parentFirstName, $socialWorkerProfileId, $swLastName, $swFirstName, $rmhStaffProfileId, $rmhStaffLastName,
+                $rmhStaffFirstName, $swDateStatusSubmitted, $rmhDateStatusSubmitted, $activityType, $status, $beginDate, $endDate,
+                $patientDiagnosis, $roomnote){
+        
             $this->roomReservationActivityID = $roomReservationActivityID;
             $this->roomReservationRequestID = $roomReservationRequestID;
-            $this->socialWorkerProfileId = $socialWorkerProfileId;
-            $this->rmhStaffProfileId = $rmhStaffProfileId;
             $this->familyProfileId = $familyProfileId;
-            $this->activityType = $activityType;
-            $this->status = $status;
+            $this->parentLastName = $parentLastName;
+            $this->parentFirstName = $parentFirstName;
+            $this->socialWorkerProfileId = $socialWorkerProfileId;
+            $this->swLastName = $swLastName;
+            $this->swFirstName = $swFirstName;
+            $this->rmhStaffProfileId = $rmhStaffProfileId;
+            $this->rmhStaffLastName = $rmhStaffLastName;
+            $this->rmhStaffFirstName = $rmhStaffFirstName;
             $this->swDateStatusSubmitted = $swDateStatusSubmitted;
             $this->rmhDateStatusSubmitted = $rmhDateStatusSubmitted;
+            $this->activityType = $activityType;
+            $this->status = $status;
             $this->beginDate = $beginDate;
             $this->endDate = $endDate;
             $this->patientDiagnosis = $patientDiagnosis;
             $this->roomnote = $roomnote;
-    }
+            }
     
     //getters
     function get_roomReservationActivityID(){
          return $this->roomReservationActivityID;
     }
+    
     function get_roomReservationRequestID(){
         return $this->roomReservationRequestID;
     }
-    function get_socialWorkerProfileId(){
-        return $this->socialWorkerProfileId;
-    }
-    function get_rmhStaffProfileId(){
-        return $this->rmhStaffProfileId;
-    }
+    
     function get_familyProfileId(){
         return $this->familyProfileId;
     }
-    function get_activityType(){
-        return $this->activityType;
+    
+    function get_parentLastName(){
+        return $this->parentLastName;
     }
-    function get_status(){
-        return $this->status;
+    
+    function get_parentFirstName(){
+        return $this->parentFirstName;
     }
+    
+    function get_socialWorkerProfileId(){
+        return $this->socialWorkerProfileId;
+    }
+    
+    function get_swLastName(){
+        return $this->swLastName;
+    }
+    
+    function get_swFirstName(){
+        return $this->swFirstName;
+    }
+    
+    function get_rmhStaffProfileId(){
+        return $this->rmhStaffProfileId;
+    }
+    
+    function get_rmhStaffLastName(){
+        return $this->rmhStaffLastName;
+    }
+    
+    function get_rmhStaffFirstName(){
+        return $this->rmhStaffFirstName;
+    }
+    
     function get_swDateStatusSubmitted(){
         return $this->swDateStatusSubmitted;
     }
-     function get_rmhDateStatusSubmitted(){
-        return $this->swdateStatusSubmitted;
+    
+    function get_rmhDateStatusSubmitted(){
+        return $this->rmhDateStatusSubmitted;
     }
+    
+    function get_activityType(){
+        return $this->activityType;
+    }
+    
+    function get_status(){
+        return $this->status;
+    }
+   
     function get_beginDate(){
         return $this->beginDate;
     }
+    
     function get_endDate(){
         return $this->endDate;
     }
-    function get_patientdiagnosis(){
+    
+    function get_patientDiagnosis(){
         return $this->patientDiagnosis;
     }
+    
     function get_roomnote(){
         return $this->roomnote;
     }
     
     
-      //setters
+    //setters
     function set_roomReservationActivityID($roomResId){
          $this->roomReservationActivityID = $roomResId;
     }
+    
     function set_roomReservationRequestID($roomRequestId){
         $this->roomReservationRequestID = $roomRequestId;
     }
-    function set_socialWorkerProfileId($sid){
-        $this->socialWorkerProfileId = $sid;
-    }
-    function set_rmhStaffProfileId($rid){
-        $this->rmhStaffProfileId = $rid;
-    }
+    
     function set_familyProfileId($famId){
         $this->familyProfileId = $famId;
     }
-    function set_activityType($actTy){
-        $this->activityType = $actTy;
+    
+    function set_parentLastName($parLname){
+        $this->parentLastName = $parLname;
     }
-    function set_status($stat){
-        $this->status = $stat;
+    
+    function set_parentFirstName($parFname){
+        $this->parentFirstName = $parFname;
     }
+    
+    function set_socialWorkerProfileId($sid){
+        $this->socialWorkerProfileId = $sid;
+    }
+    
+    function set_swLastName($sLname){
+        $this->swLastName = $sLname;
+    }
+    
+    function set_swFirstName($sFname){
+        $this->swFirstName = $sFname;
+    }
+    
+    function set_rmhStaffProfileId($rid){
+        $this->rmhStaffProfileId = $rid;
+    }
+    
+    function set_rmhStaffLastName($rLname){
+        $this->rmhStaffLastName = $rLame;
+    }
+    
+    function set_rmhStaffFirstName($rFname){
+        $this->rmhStaffFirstName = $rFname;
+    }
+    
     function set_swDateStatusSubmitted($swDateSubm){
         $this->swDateStatusSubmitted = $swDateSubm;
     }
+    
     function set_rmhDateStatusSubmitted($rmhDateSubm){
         $this->rmhDateStatusSubmitted = $rmhDateSubm;
     }
+    
+    function set_activityType($actTy){
+        $this->activityType = $actTy;
+    }
+    
+    function set_status($stat){
+        $this->status = $stat;
+    }
+   
     function set_beginDate($dateBegin){
         $this->beginDate = $dateBegin;
     }
+    
     function set_endDate($dateEnd){
         $this->endDate = $dateEnd;
     }
-    function set_patientdiagnosis($patDiagnosis){
+    
+    function set_patientDiagnosis($patDiagnosis){
         $this->patientDiagnosis = $patDiagnosis;
     }
+    
     function set_roomnote($rnote){
         $this->roomnote = $rnote;
     }
