da3e879b2bf29dede459cc33ae49b498760f931b
lindashek1@gmail.com <lindashek1@gmail.com@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-21 12:01:31 +0000

    (Modified) dbFamilyProfile.php to include update function. Some corrections to the field names were made.

--- trunk/rmh-reservation-maker/database/dbFamilyProfile.php
+++ trunk/rmh-reservation-maker/database/dbFamilyProfile.php
@@ -1,48 +1,91 @@
 <?php
+/**
+ * Functions to create, retrieve, update, and delete information from the
+ * FamilyProfile table in the database. This table is used with the family class.  
+ * @version April 20, 2012
+ * @author Weiyu Shang and Linda Shek
+ */
 
 include_once(ROOT_DIR.'/domain/Family.php');
 include_once(ROOT_DIR.'/dbinfo.php');
 
-/**DON'T CALL THIS FUNCTION BECAUSE THE TABLE ALREADY EXISTED
-function create_dbFamilyProfile() 
-{
+/**
+ * Creates a FamilyProfile table with the following fields:
+ * FamilyProfileID: primary key of the FamilyProfile table.
+ * ParentFirstName: first name of the parent. 
+ * ParentLastName: last name of the parent. 
+ * Email: email address of the parent. 
+ * Phone1: primary phone number of the parent. 
+ * Phone2: secondary phone number of the parent. 
+ * Address: parent address. 
+ * City: parent city.
+ * State: parent state. 
+ * ZipCode: parent zipcode.
+ * Country: parent country. 
+ * PatientFirstName: first name of the patient. 
+ * PatientLastName: last name of the patient. 
+ * PatientRelation: patient relation with the parent
+ * PatientDateOfBirth: Patient's date of birth
+ * FormPDF: the URL of the Form. 
+ * Notes: (optional) notes about the family.
+ */
+
+/**DO NOT CALL THE FUNCTION create_FamilyProfile() if TABLE ALREADY EXISTS. 
+function create_FamilyProfile(){
     connect();
     mysql_query("DROP TABLE IF EXISTS FamilyProfile");
-    $result = mysql_query("CREATE TABLE FamilyProfile (familyProfileId int NOT NULL, parentFirstName varchar(50),
-        parentLastName varchar(50) NOT NULL, parentEmail varchar(255), parentPhone1 varchar(20), parentPhone2 varchar(20),
-        parentAddress varchar(100), parentCity varchar(50), parentState varchar(10), parentZIP varchar(12), 
-        parentCountry(50), patientFirstName varchar(50) NOT NULL, patientLastNameemail varchar(50) NOT NULL,
-    	patientRelation varchar(50), patientBirthDate datetime, patientFormPdf varchar(255), patientNotes text)");
+    $result = mysql_query("CREATE TABLE FamilyProfile (
+        FamilyProfileID int NOT NULL AUTO_INCREMENT,
+        ParentFirstName varchar(50) NOT NULL,
+        ParentLastName varchar(50) NOT NULL,
+        Email varchar(255) DEFAULT NULL,
+        Phone1 varchar(20) NOT NULL,
+        Phone2 varchar(20) DEFAULT NULL,
+        Address varchar(100) DEFAULT NULL,
+        City varchar(50) DEFAULT NULL,
+        State varchar(10) DEFAULT NULL,
+        ZipCode varchar(12) DEFAULT NULL,
+        Country varchar(50) DEFAULT NULL,
+        PatientFirstName varchar(50) NOT NULL,
+        PatientLastName varchar(50) NOT NULL,
+        PatientRelation varchar(50) DEFAULT NULL,
+        PatientDateOfBirth datetime DEFAULT NULL,
+        FormPDF varchar(255) DEFAULT NULL,
+        Notes text,
+        PRIMARY KEY (FamilyProfileID))");
     mysql_close();
-    
-    if (!$result) 
-    {
-        echo mysql_error() . "Error creating FamilyProfile table. <br>";
+    if (!$result){
+        echo mysql_error() . ">>>Error creating FamilyProfile table. <br>";
         return false;
     }
     return true;
 }
 **/
 
+/**
+ *Inserts a new Family Profile into the FamilyProfile table. This function will be utilized by the social worker. 
+ * @param Family $family
+ * @return boolean 
+ */
 
-function insert_dbFamilyProfile ($family)
-{
-    if (! $family instanceof Family) 
-    {
+function insert_FamilyProfile ($family){
+    if (! $family instanceof Family) {
+        echo("Invaild argument for insert_FamilyProfile function call");
         return false;
     }
     connect();
 
-    $query = "SELECT * FROM FamilyProfile WHERE id = '" . $family->get_familyProfileId() . "'";
+   /* $query = "SELECT * FROM FamilyProfile WHERE id = '" . $family->get_familyProfileId() . "'";
     $result = mysql_query($query);
     if (mysql_num_rows($result) != 0) 
     {
         delete_dbFamilyProfile ($family->get_familyProfileId());
         connect();
-    }
+    }*/
 
-    $query = "INSERT INTO FamilyProfile VALUES ('".
-                $family->get_familyProfileId()."','" . 
+    $query = "INSERT INTO FamilyProfile (ParentFirstName, ParentLastName, Email, Phone1, 
+                Phone2, Address, City, State, ZipCode, Country, PatientFirstName, PatientLastName,
+                PatientRelation, PatientDateOfBirth, FormPDF, Notes) VALUES ('". 
                 $family->get_parentfname()."','".
                 $family->get_parentlname()."','".
                 $family->get_parentemail()."','".
@@ -58,39 +101,56 @@ function insert_dbFamilyProfile ($family)
                 $family->get_patientrelation()."','".
                 $family->get_patientdob()."','".
                 $family->get_patientformpdf()."','".
-                $family->get_patientnotes()."');";
+                $family->get_patientnotes()."')";
     
     $result = mysql_query($query);
-    if (!$result) 
-    {
-        echo (mysql_error(). " unable to insert into FamilyProfile: " . $family->get_familyProfileId(). "\n");
+    if (!$result) {
+        echo (mysql_error()."unable to insert into FamilyProfile: ".$family->get_familyProfileId(). "\n");
         mysql_close();
         return false;
     }
     mysql_close();
     return true;
 }
-          
-function retrieve_dbFamilyProfile ($familyProfileId) 
+   
+/**
+ * Retrieves a Family Profile from the FamilyProfile table by familyProfileId
+ * @param $familyProfileId 
+ * @return the Family Profile corresponding to familyProfileId, or false if not in the table.
+ */
+
+function retrieve_FamilyProfile ($familyProfileId) 
 {
     connect();
-    $query = "SELECT * FROM FamilyProfile WHERE id = '".$familyProfileId."'";
+    $query = "SELECT * FROM FamilyProfile WHERE id = ".$familyProfileId;
     $result = mysql_query ($query);
     
-    if (mysql_num_rows($result) !== 1)
-    {
+    if (mysql_num_rows($result) !== 1){
     	mysql_close();
         return false;
     }
     
     $result_row = mysql_fetch_assoc($result);
-    $theFamily = new Family($result_row['parentFirstName'], $result_row['parentLastName'], $result_row['parentEmail'],
-        $result_row['parentPhone1'], $result_row['parentPhone2'], $result_row['parentAddress'], $result_row['parentCity'],
-        $result_row['parentState'], $result_row['parentZip'], $result_row['parentCountry'], $result_row['patientFirstName'],
-        $result_row['patientLastName'], $result_row['patientRelation'], $result_row['patientBirthDate'],
-        $result_row['patientFormPdf'], $result_row['patientNotes']);
+    $theFamily = build_family($result_row);
+    mysql_close();
+    return $theFamily;
+}
+
+/*function retrieve_allFamilyProfile_byLname($lname){
     
-    mysql_close(); 
+}*/
+
+/* 
+ * auxiliary function to build a Family Profile from a row in the FamilyProfile table
+ */
+
+function build_family($result_row){
+    $theFamily = new Family($result_row['FamilyProfileID'],$result_row['ParentFirstName'], $result_row['ParentLastName'], $result_row['Email'],
+        $result_row['Phone1'], $result_row['Phone2'], $result_row['Address'], $result_row['City'],
+        $result_row['State'], $result_row['ZipCode'], $result_row['Country'], $result_row['PatientFirstName'],
+        $result_row['PatientLastName'], $result_row['PatientRelation'], $result_row['PatientDateOfBirth'],
+        $result_row['FormPDF'], $result_row['Notes']);
+    
     return $theFamily;
 }
 
@@ -100,7 +160,7 @@ function retrieve_dbFamilyProfile ($familyProfileId)
  * $lname = parent last name (String)
    *@return an array of Family objects
  */
-function retrieve_dbFamilyProfileByName($fname, $lname) 
+function retrieve_FamilyProfileByName($fname, $lname) 
 {
     connect();
     $query = "SELECT * FROM FamilyProfile WHERE ParentFirstName = '".$fname. "' AND ParentLastName='".$lname."'";
@@ -133,7 +193,7 @@ function retrieve_dbFamilyProfileByName($fname, $lname)
 }
 
 
-function getall_family () 
+/*function getall_family() 
 {
     connect();
     $query = "SELECT * FROM FamilyProfile ORDER BY last_name";
@@ -150,11 +210,41 @@ function getall_family ()
     }
     mysql_close();
     return $theFamily; 
-}
+}*/
 
-function update_dbFamilyProfile ($family) 
+function update_FamilyProfile ($familyProfileId) 
 {
-    if (! $family instanceof Family) 
+    connect();
+    
+    $query= "UPDATE FamilyProfile SET ParentFirstName = '".$family->get_parentfname()."', 
+            ParentLastName ='".$family->get_parentlname()."', 
+            Email = '".$family->get_parentemail()."',
+            Phone1 = '".$family->get_parentphone1()."',
+            Phone2 = '".$family->get_parentphone2()."',
+            Address = '".$family->get_parentaddress()."',
+            City = '".$family->get_parentcity()."',
+            State = '".$family->get_parentstate()."',
+            ZipCode = '".$family->get_parentzip()."',
+            Country = '".$family->get_parentcountry()."',
+            PatientFirstName = '".$family->get_patientfname()."', 
+            ParentLastName = '".$family->get_patientlname()."', 
+            PatientRelation = '".$family->get_patientrelation()."', 
+            PatientDateOfBirth = '".$family->get_patientdob()."',
+            FormPDF = '".$family->get_patientformpdf()."', 
+            Notes = '".$family->get_patientnotes()."'
+            WHERE FamilyProfileID = ".$family->$familyProfileId;
+    
+    mysql_close();
+ $result=mysql_query($query);
+   	
+    if(!$result) {
+		echo mysql_error() . ">>>Error updating FamilyProfile table. <br>";
+	    return false;
+    }  
+    return true;
+
+
+    /*if (! $family instanceof Family) 
         {
             echo ("Invalid argument for update_dbPersons function call");
             return false;
@@ -165,21 +255,26 @@ function update_dbFamilyProfile ($family)
         {
             echo (mysql_error()."unable to update FamilyProfile table: ".$family->get_familyProfileId());
 	    return false;
-	}
+	}*/
 }
 
-function delete_dbFamilyProfile($familyProfileId) 
+/**
+ * Deletes a Family Profile from the FamilyProfile table.
+ * @param $familyProfileId the id of the FamilyProfile to delete
+ */
+
+function delete_FamilyProfile($familyProfileId) 
 {
     connect();
-    $query="DELETE FROM FamilyProfile WHERE id=\"".$familyProfileId."\"";
+    $query="DELETE FROM FamilyProfile WHERE FamilyProfileID=".$familyProfileId;
 	$result=mysql_query($query);
 	mysql_close();
 	if (!$result) 
         {
-            echo (mysql_error()." unable to delete from FamilyProfile: ".$familyProfileId);
+            echo (mysql_error()."unable to delete from FamilyProfile: ".$familyProfileId);
             return false;
 	}
     return true;
 }
-
     
+?>
