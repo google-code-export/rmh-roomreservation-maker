3e6d3a0af7d201b1c0c9b596822d0671518d7209
wlodise@gmail.com <wlodise@gmail.com@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-22 12:39:45 +0000

    update to profileDetail which is still in progress. Code seems to hang on db function retrieve_FamilyProfileByName( $fn, $ln ) added profileDetail to Navigation under the option Request Room.

--- rmh-reservation-maker/family/profileDetail.php
+++ rmh-reservation-maker/family/profileDetail.php
@@ -13,69 +13,104 @@
 session_start();
 session_cache_expire(30);
 
-$title = "Family Profile in detail"; //This should be the title for the page, included in the <title></title>
+$title = "Family Profile in detail"; 
 
-include('../header.php'); //including this will further include (globalFunctions.php and config.php)
+include('../header.php'); 
 include(ROOT_DIR .'/domain/Family.php');
 include(ROOT_DIR . '/database/dbFamilyProfile.php');
 
-/*
- * If your page includes a form, please follow the instructions below.
- * If not, this code block can be deleted
- * 
- * If the page checks for $_POST data then it is important to validate the token that is attached with the form.
- * Attaching token to a form is described below in the HTML section.
- * Include the following check:
- */
-    //gets the familyid passed down by the family profile search and when the profile is selected(clicked)
-    if(isset($_GET['family']) )
+
+if(isset($_POST['form_token']) && validateTokenField($_POST) && isset($_POST['firstName']) && isset($_POST['lastName']))
     {
-      $familyID = sanitize($_GET['family']);
-      
-      $familyProfile = retrieve_dbFamilyProfile ($familyID);
-      if($familyProfile){
-        $content = '<li>'.$familyProfile->get_parentfname().'</li>';
-        $content .= '<li>'.$familyProfile->get_parentlname().'</li>';
-        $content .= '<li>'.$familyProfile->get_parentemail().'</li>';
-        $content .= '<li>'.$familyProfile->get_parentphone1().'</li>';
-        $content .= '<li>'.$familyProfile->get_parentphone2().'</li>';
-        $content .= '<li>'.$familyProfile->get_parentcity().'</li>';
-        $content .= '<li>'.$familyProfile->get_parentstate().'</li>';
-        $content .= '<li>'.$familyProfile->get_parentzip().'</li>';
-        $content .= '<li>'.$familyProfile->get_parentcountry().'</li>';
-        $content .= '<li>'.$familyProfile->get_patientfname().'</li>';
-        $content .= '<li>'.$familyProfile->get_patientlname().'</li>';
-        $content .= '<li>'.$familyProfile->get_patientrelation().'</li>';
-        $content .= '<li>'.$familyProfile->get_patientdob().'</li>';
-        $content .= '<li>'.$familyProfile->get_patientformpdf().'</li>';
-        $content .= '<li>'.$familyProfile->get_patientnotes().'</li>';
+        $fn = sanitize( $_POST['firstName']);
+        $ln = sanitize( $_POST['lastName']);
+        //echo "Post has been returned\n"; //THIS HAS BEEN REACHED
+        $families = retrieve_FamilyProfileByName($fn, $ln);//currently my code dies here
+        //an error on dbFamilyProfile.php line 165 unrecognized function connect();
         
-        $content .= '<a href="'.BASE_DIR.'/family/profileChange.php?family='.$familyID.'">Modify Profile</a>';
-        $content .= '<a href="'.BASE_DIR.'/referralForm.php?family='.$familyID.'">Create Room Reservation</a>';
-      }
-      else 
-      {
-          $content = "not found";
-      }
-                    
+        //check if famililes is an array
+        //start table
+        foreach( $families as $family )
+        {
+          //create a row with a td for lname, fname, town, dob
+          echo "A family must have been returned";//TEST POINT HAS NOT BEEN REACHED 
+        }
+        //end table
+    }
+    else if(isset($_POST['form_token']) && !validateTokenField($_POST))
+    {
+        //if the security validation failed. display/store the error:
+        //'The request could not be completed: security check failed!'
+
     }
     else
     {
-        $content = "bad request";
-    }
+        //there was no POST DATA  
+        if(isset($_GET['family']) )
+        {
+            $familyID = sanitize($_GET['family']);
+      
+            $familyProfile = retrieve_dbFamilyProfile ($familyID);
+            if($familyProfile){
+                $content = '<li>'.$familyProfile->get_parentfname().'</li>';
+                $content .= '<li>'.$familyProfile->get_parentlname().'</li>';
+                $content .= '<li>'.$familyProfile->get_parentemail().'</li>';
+                $content .= '<li>'.$familyProfile->get_parentphone1().'</li>';
+                $content .= '<li>'.$familyProfile->get_parentphone2().'</li>';
+                $content .= '<li>'.$familyProfile->get_parentcity().'</li>';
+                $content .= '<li>'.$familyProfile->get_parentstate().'</li>';
+                $content .= '<li>'.$familyProfile->get_parentzip().'</li>';
+                $content .= '<li>'.$familyProfile->get_parentcountry().'</li>';
+                $content .= '<li>'.$familyProfile->get_patientfname().'</li>';
+                $content .= '<li>'.$familyProfile->get_patientlname().'</li>';
+                $content .= '<li>'.$familyProfile->get_patientrelation().'</li>';
+                $content .= '<li>'.$familyProfile->get_patientdob().'</li>';
+                $content .= '<li>'.$familyProfile->get_patientformpdf().'</li>';
+                $content .= '<li>'.$familyProfile->get_patientnotes().'</li>';
         
+                $content .= '<a href="'.BASE_DIR.'/family/profileChange.php?family='.$familyID.'">Modify Profile</a>';
+                $content .= '<a href="'.BASE_DIR.'/referralForm.php?family='.$familyID.'">Create Room Reservation</a>';
+            }
+            else 
+            {
+                $content = "not found";
+            }
+                    
+        }
+        else
+        {
+            //$content = "bad request";
+        }
+    }
 
+    //gets the familyid passed down by the family profile search and when the profile is selected(clicked)
 ?>
 
 <div id="container">
 
     <?php include(ROOT_DIR.'/navigation.php');?>
 
-    <div id="content">
+    
+    
+    <div id="content" style="margin-left: 250px; margin-top: 23px;">
+        
+        <div id="searchForm"> <!--this div should be hidden when search results are displayed-->
+        
+        <form name="searchForm" method="POST" action="<?php echo BASE_DIR;?>/family/profileDetail.php">
+            <?php echo generateTokenField(); ?>
+            <input type="text" name="firstName" value="first name" size="50" />
+            <input type="text" name="lastName" value="last name" size="60" /><br />
+            <input type="submit" value="Lookup" />
+        </form>
+        
+        </div>
+    
+        <div id="searchResults"><!--this div will receive content after a search is performed-->
+        </div>
             <?php echo $content;?>
     </div>
 </div>
 <?php 
-include (ROOT_DIR.'/footer.php'); //include the footer file, this contains the proper </body> and </html> ending tag.
+include (ROOT_DIR.'/footer.php');
 ?>
 
--- rmh-reservation-maker/navigation.php
+++ rmh-reservation-maker/navigation.php
@@ -33,6 +33,7 @@ else if($userAccess === 1)
     //Navigation for social workers
  ?>
     <li><a href="<?php echo BASE_DIR.'/referralForm.php?id=new';?>">New Referral</a></li>
+    <li><a href="<?php echo BASE_DIR.'/family/profileDetail.php';?>">Request Room</a></li>
     <li><a href="<?php echo BASE_DIR.'/report.php';?>">Report</a></li> 
     <li><a href="<?php echo BASE_DIR.'/SearchReservations.php';?>">Search Reservations</a></li>
 <?php  
