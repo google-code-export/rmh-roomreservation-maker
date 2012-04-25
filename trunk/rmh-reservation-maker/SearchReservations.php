e1cd7050ed216333dd2dce0acc61104db2f3eba3
liyuelyly@hotmail.com <liyuelyly@hotmail.com@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-25 23:49:58 +0000

    Display searching results in a table

--- trunk/rmh-reservation-maker/SearchReservations.php
+++ trunk/rmh-reservation-maker/SearchReservations.php
@@ -9,9 +9,12 @@ include('header.php');
 include(ROOT_DIR.'/database/dbReservation.php');
 //include(ROOT_DIR.'/navigation.php');
 
+$showReservation = false;
+
     if(isset($_POST['form_token']) && validateTokenField($_POST))
     {
-        $foundReservations = searchForReservations();       
+        $showReservation = true;
+   
     }
     
     else if(isset($_POST['form_token']) && !validateTokenField($_POST))
@@ -50,6 +53,7 @@ include(ROOT_DIR.'/database/dbReservation.php');
         else if ($type=="Social Worker (Last Name)")
         {
             $socialWorkerLastName = ($_POST["searchParam"]);
+            $Reservations = array();
             $Reservations = (retrieve_SocialWorkerLastName_RoomReservationActivity($socialWorkerLastName));
             return $Reservations;
             
@@ -62,6 +66,7 @@ include(ROOT_DIR.'/database/dbReservation.php');
         else if ($type=="Staff Approver (Last Name)")
         {
             $rmhStaffLastName = ($_POST["searchParam"]);
+            $Reservations = array();
             $Reservations = (retrieve_RMHStaffLastName_RoomReservationActivity($rmhStaffLastName));
             return $Reservations;
             
@@ -74,6 +79,7 @@ include(ROOT_DIR.'/database/dbReservation.php');
         else if ($type=="Family (Last Name)")
         {
             $parentLastName = ($_POST["searchParam"]);
+            $Reservations = array();
             $Reservations = (retrieve_FamilyLastName_RoomReservationActivity($parentLastName));
             return $Reservations;
             
@@ -86,6 +92,7 @@ include(ROOT_DIR.'/database/dbReservation.php');
         else if ($type=="Status")
         {
             $status = ($_POST["searchParam"]);
+            $Reservations = array();
             $Reservations = (retrieve_RoomReservationActivity_byStatus($status));
             return $Reservations;
             
@@ -117,12 +124,105 @@ include(ROOT_DIR.'/database/dbReservation.php');
            
            <input type="text" name="searchParam" value="" size="10" />
            
+           
            <br>
            <br>
            
            <input type="submit" value="Search" name="enterSearch" />
            
        </form>
+        <?php
+        if ($showReservation == true)
+        {
+            $type = $_POST['searchType'];
+            $foundReservations = searchForReservations();
+            
+            if(empty($foundReservations))
+            {
+                echo "<br>No data matches your selections ";
+            
+            }
+            else
+            {
+       
+        echo '<br>';
+        
+        echo '<br><br>
+            <table border = "2" cellspacing = "10" cellpadding = "10">';       
+        echo '<thead>
+            <tr>
+            <th>Request ID</th>
+            <th>Staff  Name</th>
+           <th>Parent  Name</th>
+           <th>Date Status sumbitted</th>
+           <th>Begin Date</th>
+           <th>End Date</th>
+           <th>Status</th>
+           
+            </thead>
+            <tbody>';
+        
+        if($type=="Request ID")
+        {
+        $rmhRequestID= $foundReservations->get_roomReservationRequestID();
+        $rmhStaffName = $foundReservations->get_rmhStaffLastName().", ".$foundReservations->get_rmhStaffFirstName();
+       $rmhparentName= $foundReservations->get_parentLastName().", ".$foundReservations->get_parentFirstName();
+       $rmhDatasubmit = $foundReservations->get_rmhDateStatusSubmitted();
+       $rmhbeginDate=$foundReservations->get_beginDate();
+       $rmhEndDate=$foundReservations->get_endDate();
+       $rmhStatus = $foundReservations->get_status();
+       
+       echo '<tr>';
+       echo '<td>'.$rmhRequestID.'</td>';
+       echo '<td>'.$rmhStaffName.'</td>';
+       echo '<td>'.$rmhparentName.'</td>';
+       echo '<td>'.$rmhDatasubmit.'</td>';
+       echo '<td>'.$rmhbeginDate.'</td>';
+       echo '<td>'.$rmhEndDate.'</td>';
+       echo '<td>'.$rmhStatus.'</td>';
+       
+     
+      
+       echo '</tr>';
+       
+        }
+        else
+        {
+
+       foreach($foundReservations as $reservation)
+       {
+           
+        $rmhRequestID= $reservation->get_roomReservationRequestID();
+        $rmhStaffName = $reservation->get_rmhStaffLastName().", ".$reservation->get_rmhStaffFirstName();
+        $rmhparentName= $reservation->get_parentLastName().", ".$reservation->get_parentFirstName();
+        $rmhDatasubmit=$reservation->get_rmhDateStatusSubmitted();
+        $rmhbeginDate=$reservation->get_beginDate();
+        $rmhEndDate=$reservation->get_endDate();
+        $rmhStatus = $reservation->get_status();
+      
+       
+       echo '<tr>';
+       echo '<td>'.$rmhRequestID.'</td>';
+       echo '<td>'.$rmhStaffName.'</td>';
+       echo '<td>'.$rmhparentName.'</td>';
+       echo '<td>'.$rmhDatasubmit.'</td>';
+       echo '<td>'.$rmhbeginDate.'</td>';
+       echo '<td>'.$rmhEndDate.'</td>';
+       echo '<td>'.$rmhStatus.'</td>';
+      
+       
+      
+       echo '</tr>';
+       
+       
+       
+        }
+        
+        echo '</table>';
+        }
+        }
+        }
+        ?>
 
     </div>
 </div>
