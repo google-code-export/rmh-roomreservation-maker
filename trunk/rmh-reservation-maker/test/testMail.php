d5d7b0922ac8e3975274c7c5c591211f36d9b69c
alisa.modeste08@stjohns.edu <alisa.modeste08@stjohns.edu@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-25 19:27:25 +0000

    Modified sub in test email

--- trunk/rmh-reservation-maker/test/testMail.php
+++ trunk/rmh-reservation-maker/test/testMail.php
@@ -10,7 +10,7 @@ echo "<p>testing ModifyDeny, expect one SW to be emailed with patient name inclu
 ModifyDeny(2,2,2, '2/2', '3/2');
 
 echo "<p>testing email, expect email attempt to be successful<p>";
-email('alisa.modeste08@stjohns.edu', 'sub', 'mess');
+email('alisa.modeste08@stjohns.edu', 'sub Request', 'mess');
 
 echo "<p>testing ModifyAccept, expect one SW to be emailed with patient name included<p>";
 ModifyAccept(2,2,2, '2/2', '3/2');
