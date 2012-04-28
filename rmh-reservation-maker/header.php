f1992f48242480e6cb00761e0cae89ed350e50e1
prayas.bhattarai08@stjohns.edu <prayas.bhattarai08@stjohns.edu@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-28 06:03:47 +0000

    Moved the permission array and access level check to a new page "permission.php" - Pages that do not include header.php and do not usually output to the browser need to have permission check as well. To do this, the code block that checked the access level has been moved. Any handler pages that require some access level can include permission.php as an alternative to header.php

--- rmh-reservation-maker/header.php
+++ rmh-reservation-maker/header.php
@@ -10,98 +10,7 @@
     include('core/globalFunctions.php');
     include('core/sessionManagement.php');
     
-    checkSession();
-  /**
-		 * Set our permission array for families, social workers, room reservation managers, and 
-                 * administrators. If a page is not specified in the permission array, anyone logged into 
-                 * the system can view it. If someone logged into the system attempts to access a page above their
-		 * permission level, they will be sent back to the home page.
-		 */
-  
-                 //Everyone = -1
-                 //Family member = 0
-                 //Social Worker = 1
-                 //Room Reservation Manager = 2
-                 //Admin = 3
-                 
-                 //Access Level (Should match UserCategory in DB):
-                   $accessLevel = array(
-                                        'Family'=>0,
-                                        'Social Worker'=>1,
-                                        'RMH Staff Approver'=>2,
-                                        'RMH Administrator'=>3
-                                        );
-  
-                $permission_array = array();
-		//pages families can view
-		$permission_array['index.php']=0;
-                
-                $permission_array['login.php']=-1; //login page is viewable by everyone
-              
-		//additional pages social workers can view
-                
-		$permission_array['referralForm.php']=1;
-                $permission_array['profileChangeForm.php']=1;
-                $permission_array['profileChange.php']=1;
-                $permission_array['SearchReservations.php']=1;
-                $permission_array['profileDetail.php']=1;
-		//more pages
-                
-                //admin pages
-                $permission_array['listUsers.php'] = 3;
-                           
-                //additional pages room reservation managers can view
-		$permission_array['roomLog.php']=2;
-		//more pages
-         
-		//additional pages administrators can view
-		$permission_array['log.php']=3;
-		//more pages
-                
-                //password reset page (available to everyone)
-                $permission_array['reset.php']=-1;
-                
-                //logout page
-		$permission_array['logout.php']=-1;
-                
-                //reporting
-                $permission_array['report.php']=1;
-                
-                $permission_array['SearchReservations.php']=1;
-                
-                $permission_array['activity.php'] = 2;
-                $permission_array['activityHandler.php'] = 2;
-
-		//Check if they're at a valid page for their access level.
-		$current_page = getCurrentPage();
-  
-	//Log-in security
-	//If they aren't logged in, display our log-in form.
-	if(!isset($_SESSION['logged_in']) && $permission_array[$current_page] != -1){
-           //Redirect to the login page only if the current page is NOT viewable by the world AND the logged in session variable is not set
-            header('Location: '.BASE_DIR.DS.'login.php'); 
-            exit();
-	}
-        else if(isset($_SESSION['logged_in']) && ($current_page == 'login.php' || $current_page == 'reset.php'))
-        {
-            //if the current page is login.php || reset.php && the user is logged in, then redirect to the index.php page
-            header('Location: '.BASE_DIR.DS.'index.php');
-            exit();
-        }
-	else if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']){
-             //if user is logged in start the permission check
-		if(!isset($permission_array[$current_page]) || $permission_array[$current_page]>$_SESSION['access_level']){
-			//in this case, the user doesn't have permission to view this page.
-			//we redirect them to the index page.
-                        header('Location: '.BASE_DIR.DS.'index.php');
-			//echo "<script type=\"text/javascript\">window.location = \"index.php\";</script>";
-			//note: if javascript is disabled for a user's browser, it would still show the page.
-			//so we die().
-			die();
-		}
-	}
-        
-
+    include('permission.php');
 ?>
 <!DOCTYPE html>
 <html>
@@ -122,4 +31,8 @@
 <div id="header">
     <h1><?php echo $title; ?></h1>
 </div>
+        <?php
+            //show session messages
+            showSessionMessage();
+        ?>
 <?php if(isset($_SESSION['logged_in'])) include_once (ROOT_DIR.'/navigation.php'); ?>
---
+++ rmh-reservation-maker/permission.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @author Prayas Bhattarai
+ * 
+ * This file contains all the permission list for different pages.
+ * If you page is accessible by the users, access levels needs to be checked.
+ * 
+ */
+
+require_once('core/config.php');
+require_once(ROOT_DIR.'/core/sessionManagement.php');
+require_once(ROOT_DIR.'/core/globalFunctions.php');
+
+checkSession(); //check session if it has timed out or not
+
+ /* Set our permission array for families, social workers, room reservation managers, and 
+                 * administrators. If a page is not specified in the permission array, anyone logged into 
+                 * the system can view it. If someone logged into the system attempts to access a page above their
+		 * permission level, they will be sent back to the home page.
+		 */
+  
+                 //Everyone = -1
+                 //Family member = 0
+                 //Social Worker = 1
+                 //Room Reservation Manager = 2
+                 //Admin = 3
+    
+    $permission_array = array();
+    
+    /* ============ pages EVERYONE can view ===============*/
+    $permission_array['login.php']=-1; //login page is viewable by everyone
+    $permission_array['logout.php']=-1; //logout page
+    $permission_array['reset.php']=-1; //password reset page (available to everyone)
+    
+    
+    /* ============ pages users with access level of FAMILY and above can view ===============*/
+    $permission_array['index.php']=0;
+
+
+    /* ============ pages SOCIAL WORKERs and above can view ===============*/
+    $permission_array['referralForm.php']=1;
+    $permission_array['profileChangeForm.php']=1;
+    $permission_array['profileChange.php']=1;
+    $permission_array['SearchReservations.php']=1;
+    $permission_array['profileDetail.php']=1;
+    $permission_array['report.php']=1; //reporting
+    $permission_array['SearchReservations.php']=1;
+
+    
+    /* ============ pages RMH STAFF APPROVER and above can view ===============*/
+    $permission_array['activity.php'] = 2; //approval page
+    $permission_array['activityHandler.php'] = 2; //approval page handler
+
+    
+    /* ============ pages RMH ADMINISTRATOR can view ===============*/ 
+    $permission_array['listUsers.php'] = 3;
+
+    $current_page = getCurrentPage(); //current page
+    
+    //Log-in security
+    //If they aren't logged in, display our log-in form.
+    if(!isset($_SESSION['logged_in']) && $permission_array[$current_page] != -1){
+        //Redirect to the login page only if the current page is NOT viewable by the world AND the logged in session variable is not set
+        header('Location: '.BASE_DIR.DS.'login.php'); 
+        exit();
+    }
+    else if(isset($_SESSION['logged_in']) && ($current_page == 'login.php' || $current_page == 'reset.php'))
+    {
+        //if the current page is login.php || reset.php && the user is logged in, then redirect to the index.php page
+        header('Location: '.BASE_DIR.DS.'index.php');
+        exit();
+    }
+    else if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']){
+            //if user is logged in start the permission check
+            if(!isset($permission_array[$current_page]) || $permission_array[$current_page]>$_SESSION['access_level']){
+                    //in this case, the user doesn't have permission to view this page.
+                    //we redirect them to the index page.
+                    header('Location: '.BASE_DIR.DS.'index.php');
+                    //note: if javascript is disabled for a user's browser, it would still show the page.
+                    //so we die().
+                    die();
+            }
+    }
+?>
