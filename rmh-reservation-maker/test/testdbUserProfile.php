e718d3682ff1765e85c89dd481cf014785098bd2
dachosen@gmail.com <dachosen@gmail.com@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-29 20:33:38 +0000

    Added test case for the function retrieve_all_UserProfile_byRole

--- rmh-reservation-maker/test/testdbUserProfile.php
+++ rmh-reservation-maker/test/testdbUserProfile.php
@@ -39,4 +39,43 @@ function display_user($user)
     echo "Phone is " . $user->get_rmhStaffPhone() .  "</br>";
     
 }
+
+
+
+echo"</br>";
+echo "<b>Testing retrieve all user probile by by role or user category.  Will return all information for all users pertaining to that role</b>";
+test_retrieve_all_UserProfile_byRole('Social Worker');
+
+function test_retrieve_all_UserProfile_byRole($userCategory)
+{
+    $users =retrieve_all_UserProfile_byRole($userCategory);
+
+if ($users == false)
+    echo "</br>". "No user were  found";
+else
+{
+    echo  "</br>". "found  records".  "</br>";
+    foreach ($users as $user)
+    {
+         display_all_user($user);
+    }
+}
+}
+
+function display_all_user($user)
+{
+    echo"</br>";
+    echo " " . $user->get_swTitle() .  "";
+    echo" ". $user->get_swFirstName() .  "";
+    echo" ". $user->get_swLastName() .  "</br>";
+    echo " ID is " . $user->get_userProfileId() .  " ,";
+    echo "User Category is " . $user->get_userCategory () .  " ,";
+    echo "User name is " . $user->get_usernameId() .  " ,";
+    echo "Email is " . $user->get_userEmail() .  " ,";
+    echo "Password is " . $user->get_password() .  " ,";
+    echo "SW id is " . $user->get_swProfileId() .  " ,";
+    echo "Phone is " . $user->get_swphone() .  "  ,";
+    echo "Hospital Affiliation is " . $user->get_hospitalAff() .  " ,";
+    echo "Email Notification is " . $user->get_email_notification() .  "</br>";
+ }
 ?>
