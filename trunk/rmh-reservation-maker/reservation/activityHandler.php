9df7119ddd861f4bc699b465104347101b9f09eb
prayas.bhattarai08@stjohns.edu <prayas.bhattarai08@stjohns.edu@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-04-29 06:32:26 +0000

    Updated activity handler to use the update family profile function from the db function - New family profile creation activity is not addressed yet

--- trunk/rmh-reservation-maker/reservation/activityHandler.php
+++ trunk/rmh-reservation-maker/reservation/activityHandler.php
@@ -7,6 +7,7 @@ include('../permission.php'); //including this will further include (globalFunct
 include_once(ROOT_DIR.'/database/dbProfileActivity.php');
 include_once(ROOT_DIR.'/database/dbReservation.php');
 include_once(ROOT_DIR.'/database/dbFamilyProfile.php');
+include_once(ROOT_DIR.'/database/dbUserProfile.php');
 
 $errors = array();
 $messages = array();
@@ -21,11 +22,17 @@ $statuses = array('approve'=>'Confirm', 'deny'=>'Deny'); //Status info that is s
         
         $status = sanitize($_POST['status']); //whether the status was approved or denied
 
+        //since the activity tables uses RMH Staff profile ID, the current RMH profile needs to be retrieved. Maybe this kind of information can be stored in the SESSION?
+        $rmhProfile = retrieve_UserProfile_RMHApprover_OBJ(getUserProfileID());
+        $rmhStaffProfileId = $rmhProfile->get_rmhStaffProfileId();
+        
         if($requestType == 'profile')
         {
             $profileActivity = retrieve_ProfileActivity_byRequestId($requestId);
             
-            $profileActivity->set_rmhStaffProfileId(getUserProfileID());
+            
+            
+            $profileActivity->set_rmhStaffProfileId($rmhStaffProfileId);
             $profileActivity->set_rmhDateStatusSubm(date("Y-m-d H:i:s"));
             $profileActivity->set_profileActivityStatus($statuses[$status]);
             
@@ -39,34 +46,7 @@ $statuses = array('approve'=>'Confirm', 'deny'=>'Deny'); //Status info that is s
                 //if it is approved, it needs to be copied over to the family profile table
                 if(array_search($profileActivity->get_profileActivityStatus(), $statuses) == 'approve')
                 {
-                    /*Prepare data for updating the Family Profile table*/
-                    $fam_profID = $profileActivity->get_familyProfileId();
-                    $fam_parent_fname = $profileActivity->get_parentFirstName();
-                    $fam_parent_lname = $profileActivity->get_parentLastName();
-                    $fam_parent_email = $profileActivity->get_parentEmail();
-                    $fam_parent_phone_1 = $profileActivity->get_parentPhone1();
-                    $fam_parent_phone_2 = $profileActivity->get_parentPhone2();
-                    $fam_parent_addr = $profileActivity->get_parentAddress();
-                    $fam_parent_city = $profileActivity->get_parentCity();
-                    $fam_parent_state = $profileActivity->get_parentState();
-                    $fam_parent_zip = $profileActivity->get_parentZip();
-                    $fam_parent_country = $profileActivity->get_parentCountry();
-                    $fam_patient_fname = $profileActivity->get_patientFirstName();
-                    $fam_patient_lname = $profileActivity->get_patientLastName();
-                    $fam_patient_relation = $profileActivity->get_patientRelation();
-                    $fam_patient_dob = $profileActivity->get_patientDOB();
-                    $fam_form_pdf = $profileActivity->get_formPdf();
-                    $fam_notes = $profileActivity->get_familyNotes();
-
-                    if($profileActivity->get_activityType() == 'Edit')
-                    {
-                        //if the activity type is edit then update the profile
-                       $alterFamilyProfile = update_FamilyProfileDetail($fam_profID, $fam_parent_fname, $fam_parent_lname, $fam_parent_email, $fam_parent_phone_1, $fam_parent_phone_2, $fam_parent_addr, $fam_parent_city, $fam_parent_state, $fam_parent_zip, $fam_parent_country, $fam_patient_fname, $fam_patient_lname, $fam_patient_relation, $fam_patient_dob, $fam_form_pdf, $fam_notes); 
-                    }
-                    else
-                    {
-                        //if it is not edit, then it is create, so call the create function
-                    }
+                    $alterFamilyProfile = update_FamilyProfile_On_ProfileActivity($profileActivity);
                     
                     if($alterFamilyProfile)
                     {
@@ -95,7 +75,7 @@ $statuses = array('approve'=>'Confirm', 'deny'=>'Deny'); //Status info that is s
         {
             $resActivity = retrieve_RoomReservationActivity_byRequestId($requestId);
             
-            $resActivity->set_rmhStaffProfileId(getUserProfileID());
+            $resActivity->set_rmhStaffProfileId($rmhStaffProfileId);
             $resActivity->set_rmhDateStatusSubmitted(date("Y-m-d H:i:s"));
             $resActivity->set_status($statuses[$status]);
             
@@ -104,7 +84,7 @@ $statuses = array('approve'=>'Confirm', 'deny'=>'Deny'); //Status info that is s
             if($updateReservation)
             {
                 //email can be sent here
-                $messages['status_changed'] = "Reservation activity status successfuly changed";
+                $messages['status_changed'] = "Reservation activity status changed successfuly";
             }
             else
             {
