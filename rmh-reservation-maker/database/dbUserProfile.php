960ffd537577d386e72cedc99913bf67a9e475b1
dachosen@gmail.com <dachosen@gmail.com@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-05-02 17:18:33 +0000

    Added header

--- rmh-reservation-maker/database/dbUserProfile.php
+++ rmh-reservation-maker/database/dbUserProfile.php
@@ -1,4 +1,12 @@
 <?php
+/* 
+* Copyright 2011 by Alain Brutus and Bonnie MacKellar. 
+* This program is part of RMH-RoomReservationMaker, which is free software, 
+* inspired by the RMH Homeroom Project. 
+* It comes with absolutely no warranty.  You can redistribute and/or 
+* modify it under the terms of the GNU Public License as published 
+* by the Free Software Foundation (see <http://www.gnu.org/licenses/). 
+*/ 
 
 /*
  * UserProfile module for RMH Reservation Maker
