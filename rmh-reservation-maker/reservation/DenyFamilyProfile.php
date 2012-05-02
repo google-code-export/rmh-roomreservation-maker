b087f0f2bae4693cc97ff730c9ba267265ae5e1b
paul.kubler10@stjohns.edu <paul.kubler10@stjohns.edu@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-05-02 15:16:56 +0000

    Added necessary info for email

--- rmh-reservation-maker/reservation/DenyFamilyProfile.php
+++ rmh-reservation-maker/reservation/DenyFamilyProfile.php
@@ -21,8 +21,7 @@ $stat=get_status();
 $stat()='Denied';
 set_status($stat);
 
-//Submit Changes to database
-
+$reservation = retrieve_RoomReservationActivity_byRequestId($requestId);
 $familyProfileId=$reservation->get_familyProfileId(); 
 $SWID=$reservation->get_socialWorkerProfileId(); 
 
