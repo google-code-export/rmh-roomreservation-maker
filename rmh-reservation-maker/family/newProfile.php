213f8f0852f73fe96a6f283cc7493b702a0bb179
jessicacheong828@gmail.com <jessicacheong828@gmail.com@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-05-05 21:08:04 +0000

    newProfile.php button is set to Create New Profile instead of modify adjusted the new conditional statements to prevent a new profileActivity to insert into the db fixed invalid error messages, and error messages now display inside the container modified profiledetail headers

--- rmh-reservation-maker/family/newProfile.php
+++ rmh-reservation-maker/family/newProfile.php
@@ -1,8 +1,8 @@
 <?php
 
- /*
+/**
  * @author Jessica Cheong
- * @version May 1, 2012
+ * @version May 5, 2012
  * 
  * New Family Profile module for RMH-RoomReservationMaker.
  * 
@@ -10,7 +10,7 @@
  * The form is then saved in another table until the record is validated by the RMH approver. 
  * A history of the activity is stored for future references. 
  * 
- */
+*/
     
 //start the session and set cache expiry
 session_start();
@@ -43,106 +43,106 @@ include_once(ROOT_DIR .'/mail/functions.php');
         $sw_lname = $swObject->get_swLastName();
         $dateSubmit = date("Y-m-d");
         
-        if(isset($_POST['ParentFirstName'])&& $_POST['ParentFirstName'] != ""){
+        if(isset($_POST['ParentFirstName'])&& $_POST['ParentFirstName'] != "Parent First Name" && $_POST['ParentFirstName'] != ""){
             $parentfname = sanitize($_POST['ParentFirstName']);
         }
         else{
             $message['ParentFirstName'] = "Must enter Parent First Name.";
 
         }
-        if(isset($_POST['ParentLastName']) && $_POST['ParentLastName'] != ""){
+        if(isset($_POST['ParentLastName']) && $_POST['ParentLastName'] != "Parent Last Name" && $_POST['ParentLastName'] != ""){
             $parentlname = sanitize($_POST['ParentLastName']);
         }
         else{
             $message['ParentLastName'] = "Must enter Parent Last Name.";
             // print_r($message);
         }
-        if(isset($_POST['Email'])&& $_POST['Email'] != ""){
+        if(isset($_POST['Email'])&& $_POST['Email'] != "E-Mail" && $_POST['Email'] != "" ){
             $parentemail = sanitize($_POST['Email']);
           }
         else{
             $message['Email'] = "Must enter Email.";
         }
-        if(isset($_POST['Phone1'])&& $_POST['Phone1'] != ""){
+        if(isset($_POST['Phone1'])&& $_POST['Phone1'] != "Phone # 1" && $_POST['Phone1'] != ""){
             $parentphone1 = sanitize ($_POST['Phone1']);
         }
         else{
             $message['Phone1'] = "Must enter Phone1";
              //print_r($message);
         }    
-        if(isset($_POST['Phone2'])){
+        if(isset($_POST['Phone2']) && $_POST['Phone2'] != "Phone # 2" && $_POST['Phone2'] != ""){
              $parentphone2 = sanitize($_POST['Phone2']);
         }
         else{
              $parentphone2 = "";
         }
-        if(isset($_POST['Address'])&& $_POST['Address'] != ""){
+        if(isset($_POST['Address'])&& $_POST['Address'] != "Address" && $_POST['Address'] != ""){
             $parentAddr = sanitize($_POST['Address']);
         }
         else{
             $message['Address'] = "Must enter Address";
         }
-        if(isset($_POST['City'])&& $_POST['City'] != ""){
+        if(isset($_POST['City'])&& $_POST['City'] != "City" && $_POST['City'] != "" ){
              $parentcity = sanitize($_POST['City']);
         }
         else{
             $message['City'] = "Must enter City";
         }
-         if(isset($_POST['State'])&& $_POST['State'] != ""){
+         if(isset($_POST['State'])&& $_POST['State'] != "State" && $_POST['State'] != ""){
             $parentstate = sanitize($_POST['State']);
         }
         else{
             $message['State'] = "Must enter State";
         }
-         if(isset($_POST['ZipCode'])&& $_POST['ZipCode'] != ""){
+         if(isset($_POST['ZipCode'])&& $_POST['ZipCode'] != "Zip Code" && $_POST['ZipCode'] != ""){
              $parentzip = sanitize($_POST['ZipCode']);
         }
         else{
             $message['ZipCode'] = "Must enter Zip Code";
         }
-        if(isset($_POST['Country'])&& $_POST['Country'] != ""){
+        if(isset($_POST['Country'])&& $_POST['Country'] != "Country" && $_POST['Country'] != ""){
              $parentcountry = sanitize($_POST['Country']);
         }
         else{
             $message['Country'] = "Must enter Country";
         }
-         if(isset($_POST['PatientFirstName'])&& $_POST['PatientFirstName'] != ""){
+         if(isset($_POST['PatientFirstName'])&& $_POST['PatientFirstName'] != "Patient First Name" && $_POST['PatientFirstName'] != ""){
              $patientfname = sanitize($_POST['PatientFirstName']);
         }
         else{
              $message['PatientFirstName'] = "Must enter Patient First Name.";
         }
-        if(isset($_POST['PatientLastName'])&& $_POST['PatientLastName'] != ""){
+        if(isset($_POST['PatientLastName'])&& $_POST['PatientLastName'] != "Patient Last Name" && $_POST['PatientLastName'] != ""){
              $patientlname = sanitize($_POST['PatientLastName']);
         }
         else{
             $message['PatientLastName'] = "Must enter Patient Last Name";
         }
-        if(isset($_POST['PatientRelation'])&& $_POST['PatientRelation'] != ""){
+        if(isset($_POST['PatientRelation'])&& $_POST['PatientRelation'] != "Patient Relation" && $_POST['PatientRelation'] != ""){
             $patientrelation = sanitize($_POST['PatientRelation']);
         }
         else{
             $message['PatientRelation'] = " Must enter Patient Relation.";
         }
-        if(isset($_POST['PatientDOB'])&& $_POST['PatientDOB'] != ""){
+        if(isset($_POST['PatientDOB'])&& $_POST['PatientDOB'] != "Patient Date Of Birth" && $_POST['PatientDOB'] != "" ){
              $patientdob = sanitize($_POST['PatientDOB']);
         }
         else{
             $message['PatientDOB'] = "Must enter Patient Date of Birth.";
         }
-        if(isset($_POST['PatientDiagnosis'])){
+        if(isset($_POST['PatientDiagnosis']) && $_POST['PatientDiagnosis'] != "Patient Diagnosis" && $_POST['PatientDiagnosis'] != ""){
              $patientformpdf = sanitize($_POST['PatientDiagnosis']);
         }
         else{
             $patientformpdf = "";
         }
-        if(isset($_POST['PatientNote'])){
+        if(isset($_POST['PatientNote'])&& $_POST['PatientNote'] != "Patient's Notes" && $_POST['PatientNote'] != ""){
             $patientnotes = sanitize($_POST['PatientNote']);
         }
         else{
            $patientnotes = "";
         }
-        if(isset($_POST['swNote'])){
+        if(isset($_POST['swNote']) && $_POST['swNote'] != "Notes from Social Worker" && $_POST['swNote'] != ""){
             $profileActityNotes = sanitize($_POST['swNote']);
         }
         else{
@@ -152,11 +152,11 @@ include_once(ROOT_DIR .'/mail/functions.php');
 //                      var_dump($message);                               
         
         if(!empty($message)){
-            echo "unsuccessful" . "<br/>";
-            
-            foreach ($message as $messages){
-                echo $messages . "<br/>";               
-            }
+//            echo "Cannot create New Profile" . "<br/>";
+//            
+//            foreach ($message as $messages){
+//                echo $messages . "<br/>";               
+//            }
        }
         
         else if (empty($message)){
@@ -180,7 +180,7 @@ include_once(ROOT_DIR .'/mail/functions.php');
                  $profileID = $current_activity->get_profileActivityId();
                  NewFamilyProfile($profileID);
                
-                 //I should pass the familyID to room reservation script 
+                 //Need to pass the familyID to room reservation script 
                  echo '<a href="'.BASE_DIR.'/referralForm.php">Create Room Reservation</a>' . "\n";
         }       
         }
@@ -205,8 +205,19 @@ include_once(ROOT_DIR .'/mail/functions.php');
         <div>                         
         <th colspan="2"> <h2> New Family Profile Form </h2></th>
         </div>
-        <div>
-
+        
+            <div>
+                <?php 
+                if (!empty($message)){
+                    echo "Cannot create New Profile: ". "</br>";
+                foreach ($message as $messages){
+                echo $messages . "<br/>";               
+                }
+                }
+                ?>
+            </div>
+           
+            <div>
          <input class="formtop formt" type="text" name="ParentFirstName" onfocus="if(this.value == 'Parent First Name') { this.value = ''; }"  value="Parent First Name" size="40" /></div>
         <div>
 
@@ -251,7 +262,7 @@ include_once(ROOT_DIR .'/mail/functions.php');
          <input class="formt" type="text" name="PatientNote" onfocus="if(this.value == 'Patient\'s Notes') { this.value = ''; }"  value="Patient's Notes" size="40" />
          <input class="formt formbottom" type="text" name="swNote" onfocus="if(this.value == 'Notes from Social Worker') { this.value = ''; }" value="Notes from Social Worker"  size="40"/></div>
         <div>                        
-              <input class="formsubmit"type="submit" name="submit" value="Modify"/></div>
+              <input class="formsubmit"type="submit" name="submit" value="Create New Profile"/></div>
             
        </form>
 
