3e985cfe8451f51439e41dd156b9bc00799ab9c4
bkmackellar <bkmackellar@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-10-08 14:48:49 +0000

    reset to version for lab

--- Lab7-2012B/Cat.php
+++ Lab7-2012B/Cat.php
@@ -2,7 +2,7 @@
 class Cat {
     private $name;
     private $color;
-    private $breed;
+
 
     
     function setName($nm)
@@ -25,36 +25,12 @@ class Cat {
         return $this->color;
     }
     
-    function setBreed($br)
-    {
-        $this->breed = $br;
-    }
-    
-    function getBreed()
-    {
-        return $this->breed;
-    }
     
     function meow()
     {
         return $name . " says Meow!";
     }
     
-        function count()
-    {
-        $col = $_POST['color'];
-        $count = 0;
         
-        $query = "SELECT * FROM cats WHERE color='$col'";
-        $result = mysql_query ($query);
-        $theCats = array();
-        
-        while ($result_row = mysql_fetch_assoc($result))
-        {
-            $count++;
-        }
-        
-        
-    }
 }
 ?>
