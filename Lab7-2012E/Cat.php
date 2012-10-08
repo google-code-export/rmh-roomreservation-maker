b34c1f349601eed2d27ca2304a69e5877e89b872
bkmackellar <bkmackellar@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-10-08 15:01:59 +0000

    reset to version for lab

--- Lab7-2012E/Cat.php
+++ Lab7-2012E/Cat.php
@@ -2,14 +2,15 @@
 class Cat {
     private $name;
     private $color;
-   
+
+    
     function setName($nm)
     {
-       $this->name = $nm;
+        $this->name = $nm;
     }
-   
+    
     function getName()
-   {
+    {
         return $this->name;
     }
     
@@ -17,20 +18,15 @@ class Cat {
     {
         $this->color = $col;
     }
-   
-   function getColor()
+    
+    function getColor()
     {
         return $this->color;
     }
     
-    function meow($name)
+    function meow()
     {
-        return $name ." says Meow!";
-   }
-}
-    function prod_countCol($col) {
-      $result= count($col); //total count of 
-      echo $result;
+        return $name . " says Meow!";
     }
-
+}
 ?>
