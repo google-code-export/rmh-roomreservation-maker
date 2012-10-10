47afeb9a720c647d088053e11648aa0ffde993ff
troyork90 <troyork90@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-10-10 16:39:02 +0000

    with functions

--- trunk/Lab7-2012D/CatListFun.php
+++ trunk/Lab7-2012D/CatListFun.php
@@ -5,13 +5,27 @@
           $cat1->setname("Zoe");
           $myCats[] = $cat1;
           
-        $cat2 = new Cat;
+          $cat2 = new Cat;
           $cat2->setColor("orange");
           $cat2->setname("Garfield");
           $myCats[] = $cat2;
           
-           $cat3 = new Cat;
+          $cat3 = new Cat;
           $cat3->setColor("tabby");
           $cat3->setname("Fluffy");
           $myCats[] = $cat3;
+          
+          function displayCatInfo($catExample[])
+          {
+              
+              foreach ($catExample[] as $item)
+              {
+                echo "Name: " . $item->getName();
+                echo "<br>";
+                echo "Color: " . $item->getColor();
+                echo "<br>";
+                echo "Breed: " . $item->getBreed();
+                echo "<br>";
+              }
+          }
 ?>
