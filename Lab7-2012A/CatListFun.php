5a323133bc1901acd526bc2b89c74f55dc842e07
krissy88bz <krissy88bz@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-10-10 17:09:58 +0000

    -Kristen

--- Lab7-2012A/CatListFun.php
+++ Lab7-2012A/CatListFun.php
@@ -4,16 +4,19 @@ require("cat.php");
           $cat1 = new Cat;
           $cat1->setColor("black");
           $cat1->setname("Zoe");
+          $cat1->setBreed("Siamese");
           $myCats[] = $cat1;
           
         $cat2 = new Cat;
           $cat2->setColor("orange");
           $cat2->setname("Garfield");
+          $cat2->setBreed("Sphynx");
           $myCats[] = $cat2;
           
            $cat3 = new Cat;
           $cat3->setColor("tabby");
           $cat3->setname("Fluffy");
+          $cat3->setBreed("Ragdoll");
           $myCats[] = $cat3;
           
   CatListFun($myCats, "orange");
