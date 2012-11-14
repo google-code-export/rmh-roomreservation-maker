4cc942413a1b2721014bdd3adc984545d31c45b0
cdholenstein89 <cdholenstein89@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-11-14 18:29:38 +0000

    Added a back button linking the page to SeachReservations.php

--- rmh-reservation-maker/help/Reservationsearch.php
+++ rmh-reservation-maker/help/Reservationsearch.php
@@ -4,18 +4,18 @@
  */
 session_start();
 session_cache_expire(30);
-$title = 'Adding People to the Database Help Page';
+$title = 'Searching for Reservations';
 include ('../header.php');
 ?>
 
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Adding People to the Database</title>
+        <title>Search Reservations Help</title>
         <br/><br/>
     </head>
     <body>
-        <div align ="center"><h1><strong>Adding People to the Database</h1><br/>
+        <div align ="center"><h1><strong>Searching for Reservations</h1><br/>
             <ol>
                 <li>Select <i>"New Referral"</i> from the left panel.</li>
                 <li>You will now see a long form with information like <i>"First Name</i>and<i>"Last Name"</i>
@@ -36,5 +36,6 @@ include ('../header.php');
                     
                 <p><B>Step 6:</B>When you have finished you can return to any other function by selecting it on the navigation bar.<BR><BR>    
     </body>
+    <input type="submit" class="helpbutton" value="Back" align="bottom" onclick="location.href='../SearchReservations.php'" />
 </html>
 <?PHP include('../footer.php'); ?>
