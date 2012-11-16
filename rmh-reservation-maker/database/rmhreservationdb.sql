6d61884b02bea49eb0fde7e14401b6e9a88b5e28
david.elias097 <david.elias097@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-11-16 19:21:22 +0000

    -An email for testing has been changed

--- rmh-reservation-maker/database/rmhreservationdb.sql
+++ rmh-reservation-maker/database/rmhreservationdb.sql
@@ -293,7 +293,7 @@ CREATE TABLE IF NOT EXISTS `userprofile` (
 --
 
 INSERT INTO `userprofile` (`UserProfileID`, `UsernameID`, `UserEmail`, `Password`, `UserCategory`) VALUES
-(1, 'cholensstein', 'charles.holenstein08@stjohns.edu', '1bf57729257d42dd5ac2cc2214b6d5984e736b4a', 'RMH Staff Approver'),
+(1, 'delias', 'david.elias097@gmail.com', '1bf57729257d42dd5ac2cc2214b6d5984e736b4a', 'RMH Staff Approver'),
 (2, 'Mary789', 'mary1@gmail.com', '225068c27586b06e6439b2a4ff3b2a97581bffc7', 'Social Worker'),
 (3, 'jbermudez', 'Johnny.Bermudez08@stjohns.edu', '6473f3b11a9eb09f83c52a5b3a567de788f76b05', 'RMH Staff Approver'),
 (4, 'Lauren653', 'lauren1@gmail.com', '91b06a9f87b7efe1be3d56f46bc739fae897bcc5', 'Social Worker'),
