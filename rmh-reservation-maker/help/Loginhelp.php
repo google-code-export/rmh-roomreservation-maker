19822e2cefd15666a80e9319f1f39b60718c8cf5
cdholenstein89 <cdholenstein89@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-11-28 04:43:26 +0000

    Created a LoginHelp page that includes a back button to the main login screen and is contained in a <div container.

--- rmh-reservation-maker/help/Loginhelp.php
+++ rmh-reservation-maker/help/Loginhelp.php
@@ -4,23 +4,26 @@
 */
 	session_start();
 	session_cache_expire(30);
-        $title = 'Reservation Search Help';
+        $title = 'Login Help';
         include ('../header.php');
 ?>
 <html>
+    <div id="container">
+        <div id="content">
 	<head>
-		<title>
-			Homeroom login help
-		</title>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+                <title>Login Help</title>
+            <br/><br/>
 	</head>
 	<body>
-		<div align="center"><p><strong> Signing in and out of the System</strong>
+            </br></br>
+		<div align="center"><h1><strong> Signing in and out of the System</h1>
 		<p>Access to <i>Homeroom</i> requires a Username and a Password.  The form looks like this:
-		<p><table align="center">
+		<!--<p><table align="center">
 					<tr><td>Username:</td>
 			            <td><input type="text" name="user" tabindex="1"></td></tr>
 			        <tr><td>Password:</td><td><input type="password" name="pass" tabindex="2"></td></tr>
-			        <tr><td colspan="2" align="center"><input type="submit" name="Login" value="Login"></td></tr></table>
+			        <tr><td colspan="2" align="center"><input type="submit" name="Login" value="Login"></td></tr></table> -->
 		<p>Once you sign in you will be able to fill out and submit a <b>room request></b> form on-line.
 			<p>If you are a <i>volunteer, social worker, or staff member</i>, your Username is your first name followed by your phone number with no spaces.
 			<ul><li>For example, if your first name is John and your phone number is (207)-123-4567, your Username would be <strong>John2071234567</strong>.
@@ -32,6 +35,11 @@
 			</ul>
 <p> Remember to <strong>logout</strong> when you are finished using RMH Reservation Maker.
 	</body>
+        </br></br>
+           <input class="helpbutton" type="submit" value="Back" align="bottom" onclick="location.href='../login.php'" />
+        </div>
+        </div>    
+    </div>
 </html>
 <?PHP include('../footer.php'); ?>
 
