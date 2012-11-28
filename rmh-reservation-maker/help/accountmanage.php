c49d8972344d7e1364ee5c1d6e64c30ef20a36ae
cdholenstein89 <cdholenstein89@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-11-28 18:04:14 +0000

    Created an accountmange help page. It is linked to changeAccountSettings.php page.

---
+++ rmh-reservation-maker/help/accountmanage.php
@@ -0,0 +1,45 @@
+<?PHP
+/*
+ * Brian Harrison Charles Holenstein Andrew Lucas
+ */
+session_start();
+session_cache_expire(30);
+$title = 'Managing your Account';
+include ('../header.php');
+?>
+
+<html>
+    <div id="container">
+        <div id="content">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Managing your Account</title>
+        <br/><br/>
+    </head>
+    <body>
+        <div align ="center"><h1><strong>Managing your Account</h1><br/>
+            <ol>
+                <li>Select <i>"New Referral"</i> from the left panel.</li>
+                <li>You will now see a long form with information like <i>"First Name</i>and<i>"Last Name"</i>
+                   to be filled in.</li>
+                <li>When you are finished, select the <b>Submit</b> button at the bottom of the page.</li>
+                <li>next step</li>
+            </ol>
+                <p><B>Step 1:</B>Select <i>"New Referral"</i>from the left panel.<BR><BR>
+                    
+                <p><B>Step 2:</B>You will now see a long form with information like <i>"First Name</i>and<i>"Last Name"</i>
+                   to be filled in.<BR><BR>
+                
+                <p><B>Step 3:</B>When you are finished, select the <b>Submit</b> button at the bottom of the page.<Br><BR>
+                    
+                <p><B>Step 4:</B>If an error occurs you will be directed to go back and make the necessary corrections.<BR><BR>
+                    
+                <p><B>Step 5:</B>If you have no errors, all entered information will be displayed for review.<BR><BR>
+                    
+                <p><B>Step 6:</B>When you have finished you can return to any other function by selecting it on the navigation bar.<BR><BR>    
+    </body>
+    <input type="submit" class="helpbutton" value="Back" align="bottom" onclick="location.href='../changeAccountSettings.php'" />
+        </div>
+        </div>
+</html>
+<?PHP include('../footer.php'); ?>
