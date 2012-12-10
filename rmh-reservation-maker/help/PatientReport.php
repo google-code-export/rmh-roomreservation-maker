fbdfb673c84c5360ffb39ba74cdd239d4e23dd41
cdholenstein89 <cdholenstein89@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-12-10 19:55:21 +0000

    Formatted the text on the page

--- rmh-reservation-maker/help/PatientReport.php
+++ rmh-reservation-maker/help/PatientReport.php
@@ -1,6 +1,6 @@
 <?PHP
 /*
- * Brian Harrison Charles Holenstein
+ * Brian Harrison Charles Holenstein Andrew Lucas
 */
 	session_start();
 	session_cache_expire(30);
@@ -8,25 +8,35 @@
         include ('../header.php');
 ?>
 <html>
+   <div id="container">
+        <div id="content">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Patient Reports</title>
+        <title>Generating Reports</title>
+        <br/><br/>
     </head>
     <body>
-        <div align="center"><p><strong>Patient Reports</strong>
-          
-                <p><B>Step 1:</B>Select <i>"Report"</i> from the left panel.<BR><BR>
+            <div align ="left"><h1><strong>Generating a Patient Report</h1><br/>
+                <p>Select <i>"Report"</i> from the left panel.</p>
+                <p>You will now see a form with <i>"Start Date"</i> and <i> "End Date" </i></p>
+                <ol>
+                <p><b>Step 1:</b> Select the correct <i>"Start Date"</i> by selecting the correct data from each drop down menu.<BR><BR>
                     
-                <p><B>Step 2:</B>Select start and end date of stay. Then enter Hospital name to search.<BR><BR>
+                <p><B>Step 2:</B> Select the correct <i>"End Date"</i> by selecting the correct data from each drop down menu.<BR><BR>
                     
-                <p><B>Step 3:</B>If any information is left blank you will not be able to proceed until the errors are corrected.
-                    If the Hospital name is left blank it will automatically search all hospitals.<BR><BR>
+                <p><B>Step 3:</B> When you are finished entering in the criteria select the <i>"Submit"</i> button from the bottom.<BR><BR>
                     
-                <p><B>Step 4:</B>A generated report will be displayed.<BR><BR>
+                <p><B>Step 4:</B> If an error occurs you will be directed to go back and make the necessary corrections.<BR><BR>
                     
-                <p><B>Step 5:</B>When you finish you can return to any function by selecting it on the navigation bar.<BR><BR>    
+                <p><B>Step 5:</B> If you have no errors, all entered information will be displayed for review.<BR><BR>
                     
+                <p><B>Step 6:</B> When you have finished you can return to any other function by selecting it on the navigation bar on the left hand side of the page.<BR><BR>    
+                </ol>
     </body>
+    <input class="helpbutton" type="submit" value="Back" align="bottom" onclick="location.href='../report.php'" />
+        </div>
+        </div>
+    </div>
 </html>
 <?PHP include('../footer.php'); ?>
 
