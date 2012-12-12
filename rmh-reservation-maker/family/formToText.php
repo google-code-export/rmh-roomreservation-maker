d8b66f10a7c2572927d04b805d1c51e6db313b9c
stephonchap <stephonchap@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-12-12 16:21:09 +0000

    code errors for taking mailform.php and saving data to a text file for RMH staff to search with UNIX  'grep' command

--- rmh-reservation-maker/family/formToText.php
+++ rmh-reservation-maker/family/formToText.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 
-<!--
-// <?php
+ <?php
+
 /*
- * Takes mailForm data and outputs it to a textfile
- * Code errrors
- *
- * 
+* Takes mailForm data and outputs it to a textfile
+* Code errrors
+*
+* 
 *function formToText()
 *
 *$formFile='formFile.txt';
- * 
+* 
 *$formHandle = fopen(../family/mailform.php, 'a') else (" Destination not reachable")
- * 
+* 
 *$stringData= ../family/mailForm.php . " ");
- * 
+* 
 *fwrite($formHandle, $stringData);
- * 
+* 
 *fclose($formHandle);
- * 
+* 
 *?>
 */
