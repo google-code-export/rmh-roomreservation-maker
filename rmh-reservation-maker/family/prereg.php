b50d139adb10c5c86a4ff842e958654be101f9ec
tianakasky@gmail.com <tianakasky@gmail.com@2dc07112-3edd-2726-092e-4efd57c85fd4>
2012-12-12 04:55:10 +0000

    Added comments for functions

--- rmh-reservation-maker/family/prereg.php
+++ rmh-reservation-maker/family/prereg.php
@@ -13,11 +13,10 @@
 
 /*
  * This piece of code should get the randomly generated URL that is saved in the text file
- * (currently C:\wamp\www\rmh-reservation-maker\family) and get the connected family profile ID.
- * I would also like to clear the text file of the URL if I can so there isn't a problem with it
- * being duplicated
- * assuming the URL will be like:  registration.rmh-newyork.org/prereg.php?url=3928fFa4d
- * 
+ * (currently C:\wamp\www\rmh-reservation-maker\mail) and get the connected family profile ID.
+ * The URL will be like:  registration.rmh-newyork.org/prereg.php?url=3928fFa4d
+ * The only important part of the URL is the prereg.php?url=3928fFa4d, where prereg
+ * is called and the url is specified
  */
 
 if(isset($_GET["url"])) //get the url if possible. If not, set url as -1.
@@ -30,13 +29,11 @@ else
 }
 $familyID=getFamilyIDFromURL($randURL);//grabs the family ID associated with the URL
 $_SESSION['ID']=$familyID;
-if($familyID==-1)
+if($familyID==-1) //if for some reason the URL is not valid, they get taken to the index page instead.
 {
-    header( "refresh:10;url=index.php" );
-    echo "We're sorry. We could not find the form for you to fill out.<br/>";
-    echo 'You\'ll be redirected in about 10 secs. If not, click <a href="../index.php">here</a>.'; 
+    echo "We're sorry. We could not find the form for you to fill out. You should contact your social worker.<br/>";
 }
- else
+ else//redirects to stephon's form for filling out
 {
     header("refresh:5;url=mailForm.php");
     echo "You are being redirected to the form for you to fill out.<br/>";
