c5bf5c44b0a1ba62040c78f47f035fb8ed2bbb5a
prayas.bhattarai08@stjohns.edu <prayas.bhattarai08@stjohns.edu@2dc07112-3edd-2726-092e-4efd57c85fd4>
2013-01-13 23:55:16 +0000

    this update utilizes the exception class, formhelper class, data validator class, and error handler class.
    - can be used as a template for changing other pages
    - an associated file is the validationRules.php file, this includes the validation rules that are necessary for the DataValidator class. This file should, probably, contain all the validation rules for each forms in the application. If there is a form that uses the datavalidator class then corresponding fields should be mapped here (in validationRules.php) for successful data validation.

--- branches/rmh-reservation-maker-UI_Change/rmh-reservation-maker/admin/addUser.php
+++ branches/rmh-reservation-maker-UI_Change/rmh-reservation-maker/admin/addUser.php
@@ -1,20 +1,20 @@
 <?php
 /*
-* Copyright 2012 by Prayas Bhattarai and Bonnie MacKellar.
+ * Copyright 2012 by Prayas Bhattarai and Bonnie MacKellar.
 * This program is part of RMH-RoomReservationMaker, which is free software,
 * inspired by the RMH Homeroom Project.
 * It comes with absolutely no warranty.  You can redistribute and/or
 * modify it under the terms of the GNU Public License as published
 * by the Free Software Foundation (see <http://www.gnu.org/licenses/).
 */
- 
+
 /**
-* Add new user script for RMH-RoomReservationMaker. 
-* This page includes code that is used for adding a new user. 
-* The input fields are updated based on the user group that is selected by the user. This is done using javascript.
-* @author Prayas Bhattarai
-* @version May 1, 2012
-*/
+ * Add new user script for RMH-RoomReservationMaker.
+ * This page includes code that is used for adding a new user.
+ * The input fields are updated based on the user group that is selected by the user. This is done using javascript.
+ * @author Prayas Bhattarai
+ * @version Jan 13, 2013
+ */
 
 //start the session and set cache expiry
 session_start();
@@ -26,276 +26,279 @@ $helpPage = "AddaUser.php"; //help page for this view; should go before header
 include('../header.php'); //including this will further include (globalFunctions.php and config.php)
 include_once(ROOT_DIR.'/domain/UserProfile.php');
 include_once(ROOT_DIR.'/database/dbUserProfile.php');
+include_once(ROOT_DIR.'/core/class/FormHelper.php');
+include_once(ROOT_DIR.'/core/validationRules.php');
+include_once(ROOT_DIR.'/core/class/DataValidator.php');
 $errors = array();
 $messages = array();
-
+$data = array();
 $userCategories = array('admin' => 'RMH Administrator',
-    'rmhstaff' => 'RMH Staff Approver',
-    'socialworker' => 'Social Worker');
+		'rmhstaff' => 'RMH Staff Approver',
+		'socialworker' => 'Social Worker');
+$userType = 'socialworker'; //default user type
 
-if(isset($_POST['form_token']) && validateTokenField($_POST))
+if(isset($_POST['form_token']))
 {
-    //the security validation was successful, perform required operation here below.
-
-    //validate the data
-    if(isset($_POST['userGroup']))
-    {
-        $userType = sanitize($_POST['userGroup']);
-    }
-    else
-    {
-        $errors['invalid_usergroup'] = "Invalid user group";
-    }
-    if(isset($_POST['add_title']) && !empty($_POST['add_title']))
-    {
-        $add_title = sanitize($_POST['add_title']);
-    }
-    else
-    {
-        $errors['empty_title'] = "Title cannot be left empty";
-    }
-    if(isset($_POST['add_username']) && !empty($_POST['add_username']))
-    {
-        $add_username = sanitize($_POST['add_username']);
-        if(retrieve_UserByAuth($add_username))
-        {
-           $errors['duplicate_user'] = "Username already exists"; 
-        }
-    }
-    else
-    {
-        $errors['empty_username'] = "Username cannot be left empty";
-    }
-    if(isset($_POST['add_fname']) && !empty($_POST['add_fname']))
-    {
-        $add_fname = sanitize($_POST['add_fname']);
-    }
-    else
-    {
-        $errors['empty_fname']= "First name cannot be left empty";
-    }
-    if(isset($_POST['add_lname']) && !empty($_POST['add_lname']))
-    {
-        $add_lname = sanitize($_POST['add_lname']);
-    }
-    else
-    {
-        $errors['empty_lname'] = "Last name cannot be left empty";
-    }
-    if(isset($_POST['add_phone']) && !empty($_POST['add_phone']))
-    {
-        $add_phone = sanitize($_POST['add_phone']);
-    }
-    else
-    {
-        $errors['empty_phone'] = "Phone cannot be left empty";
-    }
-    if(isset($_POST['add_email']) && !empty($_POST['add_email']))
-    {
-        $add_email = sanitize($_POST['add_email']);
-    }
-    else
-    {
-        $errors['empty_email'] = "Email cannot be left empty";
-    }
-    
-    //validate data for social worker, extra info that rmh staff don't have
-    if(isset($userType) && $userType == 'socialworker')
-    {
-        if(isset($_POST['add_hospital']) && !empty($_POST['add_hospital']))
-        {
-            $add_hospital = sanitize($_POST['add_hospital']);
-        }
-        else
-        {
-            $errors['empty_hospital'] = "Hospital affiliation cannot be left empty";
-        }
-        if(isset($_POST['add_notify']) && !empty($_POST['add_notify']))
-        {
-            $add_notify = sanitize($_POST['add_notify']);
-        }
-        else
-        {
-            $errors['empty_notify'] = "Invalid notification settings";
-        } 
-    }
-    else
-    {
-        //set empty value for non-social worker user -- used in creation of user profile object
-        $add_notify = '';
-        $add_hospital = '';
-    }
-    //proceed with creating and storing the new user
-    if(empty($errors))
-    {
-        //create a default password based on: User's firstname and last 4 digits of their phone number
-        $add_password = trim(strtolower($add_fname)).trim(substr($add_phone, -4));
-        $add_password = getHashValue($add_password);
-        
-        $newUserProfile = new UserProfile($userCategories[$userType], 0, $add_username, $add_email, $add_password, 0, $add_title, $add_fname, $add_lname, $add_phone, 0, $add_title, $add_fname, $add_lname, $add_hospital, $add_phone, $add_notify);
-        
-        //insert user profile
-        $insertProfile = insert_UserProfile($newUserProfile);
-        
-        //if user profile insertion is successful, then the corresponding user profile tables need to be updated as well
-        if($insertProfile)
-        {
-            //get the userprofile id for the newly inserted user
-            //can this be done more efficiently, instead of retrieving all the info? using last_insert_id maybe?
-            $retrievedUser = retrieve_UserByAuth($add_username);
-            if($retrievedUser)
-            {
-                //if a user is retrieved, store the detailed information in the corresponding profile table             
-                $newUserProfile->set_userProfileId($retrievedUser['UserProfileID']);
-
-                if($retrievedUser['UserCategory'] == $userCategories['socialworker'])
-                {
-                    //if the user is a social worker, insert the detail info in the social worker table
-                    $insertDetailProfile = insert_SocialWorkerProfile($newUserProfile);
-                }
-                else
-                {
-                    //else the user is an rmh staff, so insert detailed profile in the rmhstaff table
-                    $insertDetailProfile = insert_RmhStaffProfile($newUserProfile);
-                }
-                
-                //check for errors
-                if($insertDetailProfile)
-                {
-                    $messages['user_creation_successful'] = "The user ".$add_username. " was successfully created.";
-                }
-                else
-                {
-                    $errors['insert_profile_detail_failure'] = "Profile detail could not be added";
-                }
-                
-            }
-            else
-            {
-                $errors['invalid_username'] = "Could not retrieve the new user";
-            }
-        }
-        else
-        {
-            $errors['insert_failed'] = "Could not add the new user";
-        }
-        
-        
-    }
-    
-    
+	$userType = sanitize($_POST['userGroup']);
+	try{
+		$validator = new DataValidator($_POST,$addUserValidationRules);
+		$data = $validator->getData();
+		if($validator->isValid()){
+			//the validation was successful, perform required operation here below.
+			$userType = $data['userGroup'];
+			$title = $data['title'];
+			
+			$username = $data['username'];
+			if(retrieve_UserByAuth($username)){
+				//if user already exists, add error message to validation field
+				$validator->setError('username', 'Username already exists');
+				throw new DuplicateUserException('Username already exists');
+			}
+			
+			$fname = $data['fname'];
+			$lname = $data['lname'];
+			$phone = $data['phone'];
+			$email = $data['email'];
+			
+			//data for social worker, extra info that rmh staff don't have
+			if(isset($userType) && $userType == 'socialworker'){
+				$hospital = $data['hospital'];
+				$notify = $data['notify'];
+			}else{
+				$hospital = '';
+				$notify = '';
+			}
+			
+			//proceed with creating and storing the new user
+			
+			//create a default password based on: User's firstname and last 4 digits of their phone number
+			$password = trim(strtolower($fname)).trim(substr($phone, -4));
+			$password = getHashValue($password);
+			
+			$newUserProfile = new UserProfile($userCategories[$userType], 0, $username, $email, $password, 0, $title, $fname, $lname, $phone, 0, $title, $fname, $lname, $hospital, $phone, $notify);
+			
+			//insert user profile
+			$insertProfile = insert_UserProfile($newUserProfile);
+				
+			//if user profile insertion is successful, then the corresponding user profile tables need to be updated as well
+			if($insertProfile)
+			{
+				//get the userprofile id for the newly inserted user
+				//can this be done more efficiently, instead of retrieving all the info? using last_insert_id maybe?
+				$retrievedUser = retrieve_UserByAuth($username);
+				if($retrievedUser)
+				{
+					//if a user is retrieved, store the detailed information in the corresponding profile table
+					$newUserProfile->set_userProfileId($retrievedUser['UserProfileID']);
+			
+					if($retrievedUser['UserCategory'] == $userCategories['socialworker'])
+					{
+						//if the user is a social worker, insert the detail info in the social worker table
+						$insertDetailProfile = insert_SocialWorkerProfile($newUserProfile);
+					}
+					else
+					{
+						//else the user is an rmh staff, so insert detailed profile in the rmhstaff table
+						$insertDetailProfile = insert_RmhStaffProfile($newUserProfile);
+					}
+			
+					//check for errors
+					if($insertDetailProfile)
+					{
+						//$messages['user_creation_successful'] = "The user ".$username. " was successfully created.";
+						setSessionMessage("The user $username was created successfully");
+						$data = array();
+						$dataErrors = array();
+					}
+					else
+					{
+						//$errors['insert_profile_detail_failure'] = "Profile detail could not be added";
+						ErrorHandler::error("Profile detail could not be added");
+					}
+			
+				}
+				else
+				{
+					//$errors['invalid_username'] = "Could not retrieve the new user";
+					ErrorHandler::error("Could not retrieve the new user");
+				}
+			}
+			else
+			{
+				//$errors['insert_failed'] = "Could not add the new user";
+				ErrorHandler::error("Could not add the new user");
+			}
+		
+		}
+	}catch(SecurityException $e){
+		//this is probably fatal?
+		ErrorHandler::error($e->getMessage());
+	}catch(DuplicateUserException $e){
+		ErrorHandler::error($e->getMessage());
+	}
 }
-else if(isset($_POST['form_token']) && !validateTokenField($_POST))
-{
-    //if the security validation failed. display/store the error:
-    //'The request could not be completed: security check failed!'
-    $errors['security_check_failed'] = 'Security check failed';
-    $userType = 'socialworker';
-}
+/* NEVER OCCURS
+ else if(isset($_POST['form_token']) && !validateTokenField($_POST))
+ {
+//if the security validation failed. display/store the error:
+//'The request could not be completed: security check failed!'
+$errors['security_check_failed'] = 'Security check failed';
+$userType = 'socialworker';
+}*/
 else if(isset($_GET['type']))
 {
-    //get requests need to be validated too. Work on validating these kinds of requests.
-
-    $userType = sanitize($_GET['type']);
-    if(!in_array($userType, array_keys($userCategories)))
-    {
-        $errors['invalid_parameter'] = 'Invalid User Category';
-        $userType = 'socialworker';
-    }
-}
-else
-{
-    //there was no POST DATA
-    $userType = 'socialworker';
+	//get requests need to be validated too. Work on validating these kinds of requests.
+	$userType = sanitize($_GET['type']);
+	if(!in_array($userType, array_keys($userCategories)))
+	{
+		ErrorHandler::error('Invalid User Category');
+	}
 }
 ?>
-
 <section class="content">
-    <div> 
-        <?php
-            if(!empty($errors))
-            {
-                echo '<div style="color:#FF3300;">';
-                echo implode('<br />', $errors);
-                echo '</div>';
-            }
-            if(!empty($messages))
-            {
-                echo '<div style="color:#00BB00;">';
-                echo implode('<br />', $messages);
-                echo '</div>';
-            }
-            else
-            {
-        ?>
-        <div class="notice">Note: Password is automatically set as the combination of user's first name and last four digits of their phone number, all lowercase.</div>
-        <form class="generic addForm" method="post" action="<?php echo BASE_DIR; ?>/admin/addUser.php">
-              			<div class="formRow">
-	                        <label for="userGroup">User Category</label>
-	                        <select class="formtop formt" id="userGroup" name="userGroup">
-	                            <option value="admin" <?php echo($userType == 'admin' ? ' selected="selected"' : null) ?>>RMH Administrator</option>
-	                            <option value="rmhstaff" <?php echo($userType == 'rmhstaff' ? ' selected="selected"' : null) ?>>RMH Staff Approver</option>
-	                            <option value="socialworker" <?php echo($userType == 'socialworker' ? ' selected="selected"' : null) ?>>Social Worker</option>
-	                        </select>
-                        </div>
-                        <?php echo generateTokenField();
-                        ?>
- 						<div class="formRow">
-                        	<label for="add_title">Title</label>
-                       		<input id="add_title" type="text" name="add_title"/>
-                       	</div>
-                       	<div class="formRow">
-                        	<label for="add_fname">First name</label>
-                        	<input id="add_fname" type="text" name="add_fname"/>
- 						</div>
-                       	<div class="formRow">
-                        	<label for="add_lname">Last name</label>
-                        	<input id="add_lname" type="text" name="add_lname"/>
-						</div>
-                       	<div class="formRow">
-                        	<label for="add_phone">Phone</label>
-                        	<input id="add_phone" type="text" name="add_phone"/>
-                        </div>
-                       	<div class="formRow">
-	                    	<label for="add_username">Username</label>
-                        	<input id="add_username" type="text" name="add_username"/>
-                       	</div>
-                       	<div class="formRow">   
-                      		<label for="add_email">Email</label>
-                       		<input id="add_email" type="text" name="add_email"/>   
-                       	</div>
-                <?php
-                if(isset($userType) && $userType == 'socialworker')
-                {
-                ?> 
-                       	<div class="formRow">
-                        	<label for="add_hospital">Hospital Affiliation</label>
-                        	<input id="add_hospital" type="text" name="add_hospital"/>
-						</div>
-                <div class="formRow">
-                       
-                       
-                        <label for="add_notify">Email Notification</label>
-                               
-                       
-                       
-                        <input type="radio" id="add_yes" name="add_notify" checked="true" value="Yes" />
-                        <label for="add_yes">Yes</label>
-                        <input type="radio" id="add_no" name="add_notify" value="No" />
-                        <label for="add_no">No</label>
-                       
-                </div>       
-                <?php
-                }
-                ?>
-                <div class="formRow">
-          			<input class="btn" type="submit" name="Submit" value="Submit" />
-          		</div>
-</form>
-        <?php } //end else if for messages
-        ?>
-</div>
-    
+	<?php ErrorHandler::displayErrors();?>
+	<div>
+		<?php
+		if(!empty($errors))
+		{
+			echo '<div style="color:#FF3300;">';
+			echo implode('<br />', $errors);
+			echo '</div>';
+		}
+		if(!empty($messages))
+		{
+			echo '<div style="color:#00BB00;">';
+			echo implode('<br />', $messages);
+			echo '</div>';
+		}
+		else
+		{
+
+			?>
+		<div class="notice">Note: Password is automatically set as the
+			combination of user's first name and last four digits of their phone
+			number, all lowercase.</div>
+		<?php
+		//error could be set elsewhere in the code, so we need retrieve it after the checks, if any
+		//in this case, we are checking for the usernam, if it is already in the database
+		$dataErrors = isset($validator) ? $validator->getErrors() : array();
+		
+		$form = new FormHelper($data, $dataErrors); //id is important, they should be unique
+		$form->create(array('class'=>array('generic','addForm'),
+				'name'=>'addNewUserForm',
+				'method'=>'post',
+				'action'=>BASE_DIR.'/admin/addUser.php'
+		));
+
+		$form->input(array(
+			'class'=>'formRow',
+			'input'=>array(
+					'type'=>'select',
+					'selected'=>$userType,
+					'id'=>'userGroup',
+					'options'=>array('admin'=>'Administrator',
+									'rmhstaff'=>'RMH Staff Approver',
+									'socialworker'=>'Social Worker'
+									)
+			),
+			'label'=>array(
+					'value'=>'User Category'
+			)
+		)
+		);
+		$form->input(array(
+				'class'=>'formRow',
+				'input'=>array(
+						'id'=>'title'
+				),
+				'label'=>array(
+						'value'=>'Title'
+				)
+		)
+		);
+
+		$form->input(array(
+					'input'=>array(
+							'id'=>'fname'
+					),
+					'label'=>array(
+							'value'=>'First Name'
+					)
+				)
+				);
+		$form->input(array(
+					'input'=>array(
+							'id'=>'lname'
+					),
+					'label'=>array(
+							'value'=>'Last Name'
+					)
+				)
+				);
+		
+		$form->input(array(
+					'input'=>array(
+							'id'=>'phone',
+							'type'=>'tel'
+					),
+					'label'=>array(
+							'value'=>'Phone'
+					)
+				)
+				);
+
+		$form->input(array(
+					'input'=>array(
+							'id'=>'username'
+					),
+					'label'=>array(
+							'value'=>'Username'
+					)
+				)
+				);
+
+		$form->input(array(
+					'class'=>'formRow',
+					'input'=>array(
+							'id'=>'email',
+							'type'=>'email'
+					),
+					'label'=>array(
+							'value'=>'Email'
+					)
+				)
+				);
+		if(isset($userType) && $userType == 'socialworker'){
+			$form->input(array(
+						'input'=>array(
+								'id'=>'hospital'
+						),
+						'label'=>array(
+								'value'=>'Hospital Affiliation'
+						)
+					)
+					);
+			
+			$form->input(array(
+						'input'=>array(
+								'id'=>'notify',
+								'selected'=>'yes',
+								'type'=>'radio',
+								'options'=>array('yes'=>'Yes',
+												'no'=>'No'
+												)
+						),
+						'label'=>array(
+								'value'=>'Email Notification'
+						)
+					)
+					);
+		}
+		$form->button();
+		$form->generate();
+	 } //end else if for messages
+?>
+	</div>
 </section>
 <script>
 <?php 
